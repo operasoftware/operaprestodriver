f211e7722ec6672cdb9bf8480fab4e9a7b03c29f
Jan Vidar Krey <janv@opera.com>
2010-05-12 21:24:16 +0200

    Fix comment.

--- src/com/opera/core/systems/IConsoleListener.java
+++ src/com/opera/core/systems/IConsoleListener.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import com.opera.core.systems.model.FilterRule;
 
 public interface IConsoleListener {
-	/*
+	/**
 	 * Map of possible filters, the callback will
 	 * only be called if the message description matches
 	 * that rule
