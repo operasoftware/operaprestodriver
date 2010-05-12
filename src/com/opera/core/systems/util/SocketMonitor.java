0e046de27aba7ae8eb6438b2fc5435a3c7f9f672
Jan Vidar Krey <janv@opera.com>
2010-05-12 21:25:25 +0200

    Make sure we only manipulate the keyset from one thread.

--- src/com/opera/core/systems/util/SocketMonitor.java
+++ src/com/opera/core/systems/util/SocketMonitor.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.channels.*;
 import java.nio.channels.spi.SelectorProvider;
 import java.util.logging.Logger;
+import java.util.LinkedList;
 
 /**
  * This will monitor any selectable channel, such as a SocketChannel or 
@@ -18,6 +19,57 @@ public class SocketMonitor {
     private final Logger logger = Logger.getLogger(this.getClass().getName());
     private Selector selector;
     private static SocketMonitor monitor = null;
+    boolean locked = false;
+
+    private enum Operation {
+        ADD, MODIFY, REMOVE
+    }
+    
+    class SelectorChangeRequest
+    {
+        protected SelectableChannel channel;
+        protected Operation op;
+        protected int mask;
+        protected SocketListener listener;
+
+        public SelectorChangeRequest(SelectableChannel channel, Operation op, int mask, SocketListener listener)
+        {
+            this.channel = channel;
+            this.op = op;
+            this.mask = mask;
+            this.listener = listener;
+        }
+
+        public SelectorChangeRequest(SelectableChannel channel, Operation op)
+        {
+            this.channel = channel;
+            this.op = op;
+            this.mask = 0;
+            this.listener = null;
+        }
+        
+        public void dump()
+        {
+            switch (op)
+            {
+                case ADD:
+                        System.out.println("SelectorChangeRequest: Add socket=" + channel.toString() + ", mask=" + mask);
+                        break;
+
+                case MODIFY:
+                        System.out.println("SelectorChangeRequest: Modify socket=" + channel.toString() + ", mask=" + mask);
+                        break;
+
+                case REMOVE:
+                        System.out.println("SelectorChangeRequest: Remove socket=" + channel.toString());
+                        break;
+            }
+        }
+
+    }
+
+    private LinkedList<SelectorChangeRequest> changes = new LinkedList<SelectorChangeRequest>();
+
     
     public static SocketMonitor instance() { 
         if (monitor == null)
@@ -36,36 +88,30 @@ public class SocketMonitor {
     }
 
     public boolean add(SelectableChannel channel, SocketListener listener, int selectMask) {
-        try {
-            logger.fine("Add channel: " + channel.toString());
-            channel.register(selector, selectMask, listener);
+        logger.fine("Add channel: " + channel.toString());
 
-            if (channel.isBlocking())
-                channel.configureBlocking(false);
-
-            return true;
-        } catch (ClosedChannelException e) {
-            e.printStackTrace(); // FIXME
-        } catch (IOException e) {
-            e.printStackTrace(); // FIXME
+        synchronized (changes) {
+            changes.add(new SocketMonitor.SelectorChangeRequest(channel, Operation.ADD, selectMask, listener));
+            selector.wakeup();
         }
-        return false;
+        return true;
     }
     
     public boolean modify(SelectableChannel channel, SocketListener listener, int selectMask) {
-        logger.fine("Modify channel: " + channel.toString());
-        try {
-            channel.register(selector, selectMask);
-            return true;
-        } catch (ClosedChannelException e) {
-            e.printStackTrace();
-            return false;
+        logger.fine("Modify channel: " + channel.toString() + ", locked=" + locked);
+         synchronized (changes) {
+            changes.add(new SocketMonitor.SelectorChangeRequest(channel, Operation.MODIFY, selectMask, listener));
+            selector.wakeup();
         }
+        return true;
     }
     
     public void remove(SelectableChannel channel) {
         logger.fine("Remove channel: " + channel.toString());
-        selector.keys().remove(channel);
+        synchronized (changes) {
+            changes.add(new SocketMonitor.SelectorChangeRequest(channel, Operation.REMOVE));
+            selector.wakeup();
+        }
     }
     
     public static boolean poll()
@@ -75,22 +121,31 @@ public class SocketMonitor {
     
     public static boolean poll(long ms)
     {
-        return instance().pollSockets(ms);
+        boolean ok = instance().pollSockets(ms);
+        instance().applyChanges();
+        return ok;
     }
     
     
-    public boolean pollSockets(long ms) {
-        logger.finest("Poll " + selector.keys().size() + " sockets");
+    protected boolean pollSockets(long ms) {
+        // logger.finest("Poll " + selector.keys().size() + " sockets");
+        
         if (selector.keys().isEmpty())
             return false;
 
+        
         try {
+            locked = true;
             selector.select(ms);
         } catch (IOException e) {
             e.printStackTrace();
             return false;
+        } finally {
+            locked = false;
         }
 
+        
+        locked = true;
         Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
         
         while (iterator.hasNext()) {
@@ -104,8 +159,55 @@ public class SocketMonitor {
                 key.cancel();
             }
         }
-        
+        locked = false;
         return true;
+    }
+    
+    protected void applyChanges()
+    {
+ 
+        synchronized (changes)
+        {
+        
+            if (changes.size() == 0)
+                return;
+
+            logger.finest("Applying " + changes.size() + " changes to selector.");
+
+            Iterator<SocketMonitor.SelectorChangeRequest> it = changes.iterator();
+            while (it.hasNext())
+            {
+                SocketMonitor.SelectorChangeRequest req = it.next();
+                req.dump();
+                switch (req.op)
+                {
+                    case ADD:
+                        try {
+                            req.channel.register(selector, req.mask, req.listener);
+                        } catch (ClosedChannelException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case MODIFY:
+                        SelectionKey key = req.channel.keyFor(selector);
+                        if (key != null)
+                        {
+                            key.interestOps(req.mask);
+                        } else {
+                            try {
+                                req.channel.register(selector, req.mask, req.listener);
+                            } catch (ClosedChannelException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        break;
+                    case REMOVE:
+                        selector.keys().remove(req.channel);
+                        break;
+                }
+            }
+            changes.clear();
+        }
     }
 
     protected void processSelectionKey(SelectionKey key) throws IOException
