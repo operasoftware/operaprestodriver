1a5ae509709114d8745444a494ded690032f810a
Jan Vidar Krey <janv@opera.com>
2010-05-12 16:01:54 +0200

    Added onConnected event.

--- src/com/opera/core/systems/scope/handlers/IConnectionHandler.java
+++ src/com/opera/core/systems/scope/handlers/IConnectionHandler.java
@@ -1,6 +1,15 @@
 package com.opera.core.systems.scope.handlers;
 
+import com.opera.core.systems.scope.stp.StpConnection;
+
 public interface IConnectionHandler {
+    
+        /**
+         * @param connection The connection that has just been connected or accepted.
+         * @return false if the connection should be closed immediately, or true to continue.
+         */
+        boolean onConnected(StpConnection connection);
+    
 	void onDisconnect();
 	
 	void onException(Exception ex);
