16f385d42fe68ad691c03184161bc4b1a6728ae7
Karianne Ekern <karie@opera.com>
2010-08-30 14:55:42 +0200

    Adding stubs for desktop test service

---
+++ src/com/opera/core/systems/scope/services/IDesktopWindowManager.java
@@ -0,0 +1,51 @@
+package com.opera.core.systems.scope.services;
+
+import java.util.List;
+//import java.util.concurrent.atomic.AtomicInteger;
+
+import org.openqa.selenium.WebDriverException;
+
+import com.opera.core.systems.scope.protos.DesktopWmProtos.DesktopWindowInfo;
+
+public interface IDesktopWindowManager {
+
+	/**
+	 * Sets the active window if the window is of type 'normal'
+	 * discards any other window such as 'dialog' or 'unknown'
+	 * @param activeWindowId
+	 */
+	void setActiveWindowId(Integer windowId);
+
+	int getActiveWindowId();
+
+	void addWindow(DesktopWindowInfo window);
+
+	void removeWindow(Integer windowId);
+	
+	void closeWindow(Integer window);
+
+	void closeAllWindows();
+
+	/**
+	 * Get the list of all windows, then find a driver window (which needs to be 'normal' window)
+	 * after that set the active window to that window
+	 */
+	void init();
+
+	/**
+	 * Find a window suitable for driving, find the active window,
+	 * if it is not a normal window, find the first normal window
+	 * via JXPath
+	 * @throws WebDriverException if no window is available for driving
+	 */
+	//void findDriverWindow();
+
+	void setActiveWindow(String title);
+	
+	List<Integer> getWindowHandles();
+	
+	int getOpenWindowCount();
+	
+	void resetWindowsList();
+
+}
