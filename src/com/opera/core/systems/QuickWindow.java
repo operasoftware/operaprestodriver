5f44f42f15aaffbe977bcba590b0e171188d240a
Karianne Ekern <karie@opera.com>
2010-09-14 12:32:46 +0200

    Cleanup

--- src/com/opera/core/systems/QuickWindow.java
+++ src/com/opera/core/systems/QuickWindow.java
@@ -5,45 +5,29 @@ import com.opera.core.systems.scope.protos.DesktopWmProtos.DesktopWindowInfo;
 public class QuickWindow {
 	
 	private final DesktopWindowInfo info; 
-	//private final OperaDriver parent;
-	//private final IOperaExec execService;
 	
-	/* (non-Javadoc)
-	 * @see com.opera.core.systems.IOperaWidget#getObjectId()
+	/* 
 	 */
 	public int getObjectId() {
 		return getWindowID();
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public int getWindowID() {
 		return info.getWindowID();
 	}
 	
 	public QuickWindow(DesktopWindowInfo info) {
-        //this.parent = parent;
         this.info = info;
-        //execService = parent.getExecService();
     }
 	
 	public String getTitle() {
 		return info.getTitle();
 	}
 
-	/*public boolean isVisible(){
-		return info.getVisible() != 0 ? true : false; 
-	}
-	private QuickWindowRect getRect() {
-		return info.getRect();
-	}
-	public Point getLocation() {
-		QuickWidgetRect rect = getRect();
-		return new Point(rect.getX(), rect.getY());
-	}
-	public Dimension getSize() {
-		QuickWidgetRect rect = getRect();
-		return new Dimension(rect.getWidth(), rect.getHeight());
-	}*/
-	
 	@Override
 	public boolean equals(Object obj) {
 		if(obj instanceof QuickWindow) {
@@ -57,7 +41,6 @@ public class QuickWindow {
 	public int hashCode() {
 		int result = 42;
 		result = 31 * result + getObjectId();
-		//result = 31 * result + runtimeId;
 		return result;
 	}
 	
