802a000228454c1cd7a024cc56c5f4f9a1b1d64b
Karianne Ekern <karie@opera.com>
2010-09-22 13:24:01 +0200

    Proper return type for QuickWidget getType

--- src/com/opera/core/systems/QuickWidget.java
+++ src/com/opera/core/systems/QuickWidget.java
@@ -139,8 +139,8 @@ public class QuickWidget {
 		 * 
 		 * @return 
 		 */
-		public int getType(){
-			return info.getType().getNumber(); 
+		public QuickWidgetType getType(){
+			return info.getType(); 
 		}
 		
 		/**
