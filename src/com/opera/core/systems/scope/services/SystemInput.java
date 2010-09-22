123c914029d8224fbfcba9e46601ffe6af231107
Karianne Ekern <karie@opera.com>
2010-09-22 09:16:13 +0200

    Added more mouse commands to SystemInput

--- src/com/opera/core/systems/scope/services/SystemInput.java
+++ src/com/opera/core/systems/scope/services/SystemInput.java
@@ -5,5 +5,8 @@ import java.awt.Point;
 public interface SystemInput {
 		void click(Point location, int button, int num_clicks, int modifier);
 		void keyPress(String key, int modifier);
+		void mouseMove(Point location, int button, int modifier);
+		void mouseUp(Point location, int button, int modifier);
+		void mouseDown(Point location, int button, int modifier);
 }
 
