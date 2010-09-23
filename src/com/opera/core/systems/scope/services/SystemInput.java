42ef1fd4e9ce517d9ff1db2313319fad9f9a16c3
Karianne Ekern <karie@opera.com>
2010-09-23 12:40:14 +0200

    Regenerated SystemInputProtos and adjusted SystemInput manager

--- src/com/opera/core/systems/scope/services/SystemInput.java
+++ src/com/opera/core/systems/scope/services/SystemInput.java
@@ -1,15 +1,16 @@
 package com.opera.core.systems.scope.services;
 
 import java.awt.Point;
+import java.util.List;
 
 import com.opera.core.systems.scope.protos.SystemInputProtos.ModifierPressed;
 import com.opera.core.systems.scope.protos.SystemInputProtos.MouseInfo.MouseButton;
 
 public interface SystemInput {
-		void click(Point location, MouseButton button, int num_clicks, ModifierPressed modifier);
-		void keyPress(String key, ModifierPressed modifier);
-		void mouseMove(Point location, MouseButton button, ModifierPressed modifier);
-		void mouseUp(Point location, MouseButton button, ModifierPressed modifier);
-		void mouseDown(Point location, MouseButton button, ModifierPressed modifier);
+		void click(Point location, MouseButton button, int num_clicks, List<ModifierPressed> modifiers);
+		void keyPress(String key, List<ModifierPressed> modifiers);
+		void mouseMove(Point location, MouseButton button, List<ModifierPressed> modifiers);
+		void mouseUp(Point location, MouseButton button, List<ModifierPressed> modifiers);
+		void mouseDown(Point location, MouseButton button, List<ModifierPressed> modifiers);
 }
 
