f3950013aeb186669616b428a57ff86772fe364e
James Livermont <jamesl@opera.com>
2010-12-07 10:04:26 +0100

    Latest version of unit tests and code refactoring (rest of the files)

--- test/ScopeServicesTest.java
+++ test/ScopeServicesTest.java
@@ -8,33 +8,33 @@ import com.opera.core.systems.*;
 import com.opera.core.systems.runner.*;
 import com.opera.core.systems.runner.launcher.*;
 import com.opera.core.systems.settings.*;
+
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.Platform;
 
 public class ScopeServicesTest extends TestCase 
 {
 	static ScopeServices services;
+	static Map<String, String> versions;
 	static OperaDriverSettings settings;
 	static OperaLauncherRunner runner;	
 	
-	public void testOperaAndLauncherInitializationForServices()
+	public void testInitializeRunnerAndOpera()
 	{
 		settings = new OperaDriverSettings();
 		settings.setRunOperaLauncherFromOperaDriver(true);
 		settings.setOperaBinaryLocation(System.getProperty("test_gogi_binary_location"));
-		settings.setOperaLauncherBinary(System.getProperty("test_launcher_binary_location"));
-		settings.setOperaBinaryArguments(" -geometry 800x600 -nohw");
-		
+		settings.setOperaLauncherBinary(System.getProperty("test_launcher_binary_location"));		
+		settings.setOperaBinaryArguments(" -geometry 1280x1024 -nohw -url www.opera.com");
 		runner = new OperaLauncherRunner(settings);
 		runner.startOpera();
-		
-		Assert.assertNotNull(runner);
+		Assert.assertTrue(runner.isOperaRunning());			
 	}
 	
 	public void testScopeServicesConstructor()
 	{
 		// these versions may become deprecated in the future, but should give a good backwards compatability test
-		Map<String, String> versions = new HashMap<String, String>();
+		versions = new HashMap<String, String>();
 		versions.put("ecmascript-debugger", "5.0");
 		versions.put("window-manager", "2.0");
 		versions.put("exec", "2.0");
@@ -52,10 +52,20 @@ public class ScopeServicesTest extends TestCase
 		Assert.assertNotNull(services);
 	}
 	
+	public void testStartSTPThread()
+	{				
+		try
+		{
+			services.startStpThread();			
+		}
+		catch (WebDriverException e)
+		{	
+			fail("WebDriverException encountered while starting the STP thread");
+		}					
+	}
+	
 	public void testInitializeScopeServices()
-	{		
-		services.startStpThread();
-		
+	{	
 		try
 		{
 			services.init();			
@@ -63,23 +73,52 @@ public class ScopeServicesTest extends TestCase
 		catch (WebDriverException e)
 		{	
 			fail("WebDriverException encountered while initializing Scope Services");
-			e.printStackTrace();
 		}					
-	}
+	}	
 	
 	public void testIsConnected()
-	{		
+	{
 		Assert.assertTrue(services.isConnected());
 	}
 	
-	public void testShutdown()
+	// this is effectively a test to see if the core service is available or not
+	public void testIsOperaIdleAvailable()
 	{
-		services.shutdown();
+		Assert.assertTrue(services.isOperaIdleAvailable());		
+	}
+	
+	public void testIsSTPConnectionObjectNull()
+	{
+		Assert.assertNotNull(services.getConnection());
+	}
+	
+	public void testGetListedServices()
+	{
+		List<String> list = services.getListedServices();
+		Assert.assertTrue(list.size() > 0);
+	}
+	
+	public void testIsExecServiceAvailable()
+	{
+		List<String> list = services.getListedServices();
+		Assert.assertTrue(list.contains("exec"));
+	}
+	
+	public void testIsWindowManagerServiceAvailable()
+	{
+		List<String> list = services.getListedServices();
+		Assert.assertTrue(list.contains("window-manager"));
+	}	
+	
+	public void testShutdownScopeServices()
+	{
+		services.quit();
 		Assert.assertFalse(services.isConnected());
-		
-		// now shutdown the launcher and Opera
+	}
+	
+	public void testCloseOperaAndRunner()
+	{
 		runner.stopOpera();
 		runner.shutdown();
-	}
-
+	}	
 }
