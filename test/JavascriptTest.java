3fa2749bcc1eb173e96815206fc1d02d66b7f9c7
Stuart Knightley <stuartk@opera.com>
2010-12-09 10:41:18 +0100

    Added/updated typing and clicking tests

--- test/JavascriptTest.java
+++ test/JavascriptTest.java
@@ -11,13 +11,33 @@ import com.opera.core.systems.model.FilterRule;
 
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebElement;
 
 public class JavascriptTest extends TestCase
 {
   private static OperaDriverSettings settings;
   private static OperaDriver driver;
 
-  public JavascriptTest()
+  private void loadFixture(String filename)
+  {
+    String path = getClass().getProtectionDomain().getCodeSource().getLocation()+
+      "fixtures/"+filename;
+
+    // Won't know if this works until OperaDriver works
+    driver.get(path);
+  }
+
+  public void setUp()
+  {
+    if (driver != null)
+    {
+      loadFixture("javascript.html");
+      // Click to focus the document
+      driver.mouseEvent(1, 1, 1);
+    }
+  }
+
+  public void testInit()
   {
     settings = new OperaDriverSettings();
     settings.setRunOperaLauncherFromOperaDriver(true);
@@ -28,54 +48,32 @@ public class JavascriptTest extends TestCase
     driver = new OperaDriver(settings);
   }
 
-  private void loadFixture(String filename)
-  {
-    // Won't know if this works until OperaDriver works
-    driver.get(getClass().getProtectionDomain().getCodeSource().getLocation()+
-      File.pathSeparator+"fixtures/"+filename
-    );
-  }
-
-  //@Before
-  public void setup()
-  {
-    loadFixture("javascript.html");
-  }
-
   public void testTyping()
   {
     String text = "Hello, world!";
 
     driver.executeScript("document.getElementById('one').focus()");
     driver.type(text);
-
-    Assert.assertEquals(driver.findElementById("one").getValue(), text);
+    try { Thread.sleep(1000); } catch (Exception e) {}
+    Assert.assertEquals(text, driver.findElementById("one").getValue());
   }
 
   // Make sure that typing actually happens. When the focus switches half way
   // through typing we should continue typing on the other textbox
-  public void testTypingReal()
-  {
-    // Changes text field focus after 200ms
-    driver.executeScript("autofocus()");
-    driver.type("abcdefghijklmnopqrstuvwxyz");
-
-    Assert.assertTrue(driver.findElementById("one").getValue().startsWith("ab"));
-    Assert.assertTrue(driver.findElementById("two").getValue().endsWith("yz"));
-  }
-
   public void testTypingKeyEvents()
   {
     loadFixture("keys.html");
-    driver.type("one");
 
-    //String[] log = driver.findElementById("log").text.split("\n");
-    //Assert.assertEquals(log.length, 9);
+    driver.type("hi");
 
+    String log = driver.findElementById("log").getValue();
+    Assert.assertTrue(log.contains("press, 104, h,"));
+    Assert.assertTrue(log.contains("up, 73, I,"));
   }
 
   // TODO finish this test
-  public void testConsoleListener()
+  /*
+oper  public void testConsoleListener()
   {
     driver.addConsoleListener(new IConsoleListener() {
       public List<FilterRule> getFilters() {
@@ -85,8 +83,22 @@ public class JavascriptTest extends TestCase
       }
 
       public void onMessage(String message) {
-        System.out.println("!!!"+message);
+        Assert.assertEquals("one", message);
       }
     });
+  }
+  */
+
+  public void testDoubleClick()
+  {
+    WebElement one = driver.findElementById("one");
+    one.click();
+    one.click();
+    Assert.assertEquals(driver.findElementById("two").getValue(), "double");
+  }
+
+  public void testShutDownOperaDriver()
+  {
+	  driver.shutdown();
   }
 }
