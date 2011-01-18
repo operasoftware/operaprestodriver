af3c108da4879154a070f1242d8c392ca315a3d7
Stuart Knightley <stuartk@opera.com>
2011-01-18 14:31:00 +0100

    Moves setup for desktop tests into DesktopTestBase, and moves settings setup into own public function

--- test/com/opera/core/systems/SecurityDialogs.java
+++ test/com/opera/core/systems/SecurityDialogs.java
@@ -4,54 +4,40 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.opera.core.systems.settings.OperaDriverSettings;
+public class SecurityDialogs extends DesktopTestBase {
+  @Test
+  public void testShowSecurityInfo() {
+    driver.waitStart();
 
-public class SecurityDialogs extends TestBase {
-  private static OperaDesktopDriver driver;
+    driver.operaDesktopAction("Open url in new page",
+        "http://t/security/bts/164110/scary_file-keyboard.kini");
 
-  @BeforeClass
-  public static void setUpBeforeClass() throws Exception {
-    OperaDriverSettings settings = new OperaDriverSettings();
-    // TODO make work everywhere
-    settings.setOperaBinaryLocation(binary);
-    settings.setOperaLauncherBinary(launcher);
+    // Add try-catch for timeout
+    int win_id = driver.waitForWindowShown("Setup Apply Dialog Confirm Dialog");
 
-    driver = new OperaDesktopDriver(settings);
+    QuickWindow security_win = driver
+        .getWindow("Setup Apply Dialog Confirm Dialog");
+    assertTrue(win_id == security_win.getWindowID());
+
+    List<QuickWindow> windows = driver.getWindowList();
+    List<QuickWidget> widgets = driver
+        .getQuickWidgetList("Setup Apply Dialog Confirm Dialog");
+
+    assertTrue("Server is t", driver.findWidgetByName(win_id, "Simple_message")
+        .getText().startsWith("t"));
+    assertTrue("Has security warning message", driver.findWidgetByName(win_id,
+        "Simple_message").verifyContainsText("D_SECURITYALERT_SETUPDOWNLOAD"));
+
+    QuickWidget no_btn = driver.findWidgetByName(win_id, "button_Cancel");
+    QuickWidget yes_btn = driver.findWidgetByName(win_id, "button_Ok");
+    assertTrue("Yes is not default button", yes_btn != null
+        && !yes_btn.isDefault());
+    assertTrue("No is default button", no_btn.isDefault());
+
+    driver.operaDesktopAction("Cancel");
+    // driver.keyPress("F1", ModifierPressed.NONE);
+    // no_btn.click(MouseButton.LEFT, 1, ModifierPressed.NONE);
   }
-
-  // FIXME shutdown driver
-  @AfterClass
-  public static void tearDownAfterClass() throws Exception {}
-
-	    @Test
-	    public void testShowSecurityInfo(){
-	        driver.waitStart();
-
-	        driver.operaDesktopAction("Open url in new page", "http://t/security/bts/164110/scary_file-keyboard.kini");
-
-	        // Add try-catch for timeout
-	    	int win_id = driver.waitForWindowShown("Setup Apply Dialog Confirm Dialog");
-
-	    	QuickWindow security_win = driver.getWindow("Setup Apply Dialog Confirm Dialog");
-	    	assertTrue(win_id == security_win.getWindowID());
-
-	    	List<QuickWindow> windows = driver.getWindowList();
-	    	List<QuickWidget> widgets = driver.getQuickWidgetList("Setup Apply Dialog Confirm Dialog");
-
-		    assertTrue("Server is t", driver.findWidgetByName(win_id, "Simple_message").getText().startsWith("t"));
-			assertTrue("Has security warning message", driver.findWidgetByName(win_id, "Simple_message").verifyContainsText("D_SECURITYALERT_SETUPDOWNLOAD"));
-
-		    QuickWidget no_btn = driver.findWidgetByName(win_id, "button_Cancel");
-		    QuickWidget yes_btn = driver.findWidgetByName(win_id, "button_Ok");
-		    assertTrue("Yes is not default button", yes_btn != null && !yes_btn.isDefault());
-		    assertTrue("No is default button", no_btn.isDefault());
-
-		    driver.operaDesktopAction("Cancel");
-		    //driver.keyPress("F1", ModifierPressed.NONE);
-		    //no_btn.click(MouseButton.LEFT, 1, ModifierPressed.NONE);
-	    }
 }
