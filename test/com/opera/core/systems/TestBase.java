d3a78b8a910a0400e814fdb64f0e41bf75c4cd2e
Stuart Knightley <stuartk@opera.com>
2011-01-18 11:42:57 +0100

    Makes a couple of vars protected in OperaDriver, so they can be accessed in the added TestBase

---
+++ test/com/opera/core/systems/TestBase.java
@@ -0,0 +1,74 @@
+package com.opera.core.systems;
+
+import java.io.File;
+
+import junit.framework.Assert;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+import com.opera.core.systems.runner.OperaRunner;
+import com.opera.core.systems.settings.OperaDriverSettings;
+
+abstract public class TestBase {
+  protected static TestOperaDriver driver;
+  protected static OperaDriverSettings settings;
+
+  protected static String binary = "/usr/bin/opera";
+  protected static String launcher = "/home/stuart/bin/opera-launcher";
+
+  private static String fixture_dir;
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+    // Start up the driver
+    OperaDriverSettings settings = new OperaDriverSettings();
+    settings.setRunOperaLauncherFromOperaDriver(true);
+    // TODO make work everywhere
+    settings.setOperaBinaryLocation(binary);
+    settings.setOperaLauncherBinary(launcher);
+
+    driver = new TestOperaDriver(settings);
+    Assert.assertNotNull(driver);
+
+    // Setup the fixture directory
+    String separator = System.getProperty("file.separator");
+    fixture_dir = System.getProperty("user.dir");
+    fixture_dir = fixture_dir + separator + "test" + separator + "fixtures"
+        + separator;
+
+    Assert.assertTrue(new File(fixture_dir).isDirectory());
+  }
+
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {
+    driver.shutdown();
+  }
+
+  // Easy access to fixtures
+
+  /// Get the URL of the given fixture file
+  protected String fixture(String file) {
+    return fixture_dir + file;
+  }
+
+  /// Navigate to the given fixture file
+  protected void getFixture(String file) {
+    driver.get(fixture(file));
+  }
+}
+
+// Provides access to the Opera Runner, so we can detect crashes
+class TestOperaDriver extends OperaDriver {
+  public TestOperaDriver(OperaDriverSettings settings) {
+    super(settings);
+  }
+
+  public OperaRunner getRunner() {
+    return operaRunner;
+  }
+
+  public OperaDriverSettings getSettings() {
+    return settings;
+  }
+}
