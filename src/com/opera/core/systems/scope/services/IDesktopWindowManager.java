c9fa11acf990ef42ab5209b70c75a6d5842b0eab
Karianne Ekern <karie@opera.com>
2011-01-24 09:44:54 +0100

    getQuickWindow -> getQuickWindowByName (for consistency with other methods)

--- src/com/opera/core/systems/scope/services/IDesktopWindowManager.java
+++ src/com/opera/core/systems/scope/services/IDesktopWindowManager.java
@@ -10,76 +10,71 @@ import com.opera.core.systems.scope.protos.DesktopWmProtos.QuickWidgetSearch.Qui
 public interface IDesktopWindowManager {
 
 	/**
-	 * Does initalization neccessary to set up the service
+	 * Does initalization neccessary to set up the service.
 	 */
 	void init();
 
 	/**
-	 * Gets id of active window
-	 * 
+	 * Gets id of active window.
+	 *
 	 * @return id of active window
 	 */
 	int getActiveQuickWindowId();
 
 	/**
-	 * Gets number of open windows
-	 * 
+	 * Gets number of open windows.
+	 *
 	 * @return number of open windows
 	 */
 	int getOpenQuickWindowCount();
 
-	/*
+	/**
+	 * Gets list of all QuickWidgets in the window.
+	 *
+	 * @param windowId id of window
 	 * @return list of QuickWidgets in the window given by id id
 	 */
 	List<QuickWidget> getQuickWidgetList(int windowId);
 
 	/**
-	 * Gets the window with name name
-	 * 
-	 * @param title
-	 * @return QuickWindow
-	 */
-	QuickWindow getQuickWindow(String name);
-
-	/**
-	 * Gets window id of this window
+	 * Gets window id of window with the given name
 	 *
-	 * @param title
-	 * @return id of window with the given title
+	 * @param name window name
+	 * @return id of window with the given name
 	 */
-	int getQuickWindowID(String title);
+	int getQuickWindowID(String name);
 
 	/**
-	 * Gets a list of all open windows
+	 * Gets a list of all open windows.
 	 *
 	 * @return list of all open windows
 	 */
 	List<QuickWindow> getQuickWindowList();
 
 	/**
-	 * Gets the name of the window
-	 * 
-	 * @param windowId
+	 * Gets the name of the window with windowId.
+	 *
+	 * @param windowId id of window
 	 * @return name of the window
 	 */
 	String getQuickWindowName(int windowId);
 
 	/**
-	 * Gets a QuickWidget based on a specific property
+	 * Gets a QuickWidget based on a specific property.
 	 *
-	 * @param windowId - window id of the window containing the widget
-	 * @param property - property specified for the search 
-	 * @param value - value of property given
+	 * @param windowId window id of the window containing the widget
+	 * @param property property specified for the search
+	 * @param value value of property given
 	 * @return QuickWidget matching the property, or null if no such widget
 	 */
 	QuickWidget getQuickWidget(int windowId, QuickWidgetSearchType property, String value);
-	
+
 	/**
-	 * Gets the quick widget in a specified window based on a specific property
-	 * and its parent widgets name
+	 * Gets the quick widget in a specified window based on a specific
+	 * property and its parent widgets name.
 	 *
 	 * @param windowId - window id of the window containing the widget
-	 * @param property - property specified for the search, NAME, POS or TEXT
+	 * @param property - property specified for the search
 	 * @param value - value of property given
 	 * @param parentName - Name of the widgets parent widget
 	 * @return QuickWidget matching the property, or null if no such widget
@@ -87,7 +82,7 @@ public interface IDesktopWindowManager {
 	QuickWidget getQuickWidget(int windowId, QuickWidgetSearchType property, String value, String parentName);
 
 	/**
-	 * Gets the quick widget based on its position
+	 * Gets the quick widget based on its position.
 	 *
 	 * @param windowId - window id of the window containing the widget
 	 * @param row - row of the widget
@@ -97,8 +92,8 @@ public interface IDesktopWindowManager {
 	QuickWidget getQuickWidgetByPos(int windowId, int row, int column);
 
 	/**
-	 * Gets the quick widget based on its position and its parent widgets
-	 * name
+	 * Gets the quick widget based on its position and
+	 * its parent widgets name.
 	 *
 	 * @param windowId - window id of the window containing the widget
 	 * @param row - row of the widget
@@ -109,7 +104,7 @@ public interface IDesktopWindowManager {
 	QuickWidget getQuickWidgetByPos(int windowId, int row, int column, String parentName);
 
 	/**
-	 * Gets the quick window based on a specific property
+	 * Gets the quick window based on a specific property.
 	 *
 	 * @param property - property to search by
 	 * @param value - value of property given
@@ -118,10 +113,18 @@ public interface IDesktopWindowManager {
 	QuickWindow getQuickWindow(QuickWidgetSearchType property, String value);
 
 	/**
-	 * Gets the quick window with the given window id 
+	 * Gets the quick window with the given window id.
 	 *
 	 * @param windowId - id of the window
 	 * @return QuickWindow with id windowId, if any, else null. 
 	 */
 	QuickWindow getQuickWindowById(int windowId);
+	
+	/**
+	 * Gets the window with name name.
+	 *
+	 * @param name window name
+	 * @return QuickWindow
+	 */
+	QuickWindow getQuickWindowByName(String name);
 }
