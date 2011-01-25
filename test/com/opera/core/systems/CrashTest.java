e625178154d824dba9badbd76d277dcd00c3a96e
Stuart Knightley <stuartk@opera.com>
2011-01-25 15:47:22 +0100

    Add custom setup and tear down methods

--- test/com/opera/core/systems/CrashTest.java
+++ test/com/opera/core/systems/CrashTest.java
@@ -1,10 +1,32 @@
 package com.opera.core.systems;
 
 import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-// TODO make tests pass
+// FIXME make tests pass
 public class CrashTest extends TestBase {
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {}
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {}
+
+  @Before
+  public void setUp() throws Exception {
+    super.setUpBeforeClass();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    super.tearDownAfterClass();
+    // Make sure Opera is gone
+    Runtime.getRuntime().exec("kill `pgrep opera`").waitFor();
+  }
+
   @Test
   public void testCore32291() {
     driver.get("http://t/core/bts/crashers/visual/CORE-32291/001.html");
