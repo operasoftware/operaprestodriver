a435810925b0fbcd0053ae85a56ef78b31a764ee
Stuart Knightley <stuartk@opera.com>
2011-02-02 11:48:01 +0100

    Add javadoc

--- src/com/opera/core/systems/OperaPaths.java
+++ src/com/opera/core/systems/OperaPaths.java
@@ -9,6 +9,16 @@ import org.openqa.selenium.Platform;
  */
 public class OperaPaths {
 
+  /**
+   * This method will try and find Opera on any system. It takes the following
+   * steps:
+   *  1. Check the environment variable "OPERA_PATH". If it exists, and the
+   *     file it points to exists, then return
+   *  2. (Unix) Call `which opera` to find the location
+   *  3. Check if Opera exists at the default location on the respective OS
+   *  4. Give up and return null
+   * @return The path to Opera, or null
+   */
   public String operaPath() {
     String path;
     if ((path = System.getenv("OPERA_PATH")) != null) return path;
@@ -30,8 +40,23 @@ public class OperaPaths {
     return null;
   }
 
+  /**
+   * This method will try and find Opera Launcher on any system.
+   * @return The path to Opera Launcher, or null
+   */
   public String launcherPath() {
     return "/home/stuart/bin/opera-launcher";
+  }
+
+  /**
+   * Check that the given path is not null, and exists.
+   * @param path The path to check.
+   * @return null if the path is null, or does not exist. Otherwise it returns
+   * the given path.
+   */
+  private String checkPath(String path) {
+    if (path != null && new File(path).exists()) return path;
+    else return null;
   }
 
 }
