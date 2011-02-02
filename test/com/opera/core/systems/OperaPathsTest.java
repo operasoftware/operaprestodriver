8530636cdd60601447107d8d19108a2a5026524f
Stuart Knightley <stuartk@opera.com>
2011-02-02 11:27:54 +0100

    Get a URL that does not redirect to avoid a bug

--- test/com/opera/core/systems/OperaPathsTest.java
+++ test/com/opera/core/systems/OperaPathsTest.java
@@ -4,26 +4,15 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import com.opera.core.systems.settings.OperaDriverSettings;
-
 public class OperaPathsTest {
 
   @Test
-  public void testStart() throws Exception {
-    OperaDriverSettings settings = new OperaDriverSettings();    
-    settings.setRunOperaLauncherFromOperaDriver(true);
-
-    // TODO make work everywhere
-    settings.setOperaBinaryLocation("/usr/bin/opera");
-    settings.setOperaLauncherBinary("/home/stuart/bin/opera-launcher");
-
-    // FIXME -nosession doesn't work on Windows
-    settings.setOperaBinaryArguments("opera:debug");    
+  public void testStart() throws Exception {   
+    OperaDriver driver = new OperaDriver();
     
-    OperaDriver driver = new OperaDriver(settings);
-    driver.get("http://example.com/");
+    driver.get("http://t/core/README");
     
-    assertEquals("http://example.com/", driver.getCurrentUrl());
+    assertEquals("http://t/core/README", driver.getCurrentUrl());
     
     driver.shutdown();
   }
