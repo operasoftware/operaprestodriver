16c38345a42f59403be206097ca99c7884dee8d7
Deniz Turkoglu <denizt@opera.com>
2011-02-09 13:06:54 +0100

    Added more checks and platforms to OperaPaths

--- src/com/opera/core/systems/OperaPaths.java
+++ src/com/opera/core/systems/OperaPaths.java
@@ -46,56 +46,56 @@ public class OperaPaths {
    * steps:
    *  1. Check the environment variable "OPERA_PATH". If it exists, and the
    *     file it points to exists, then return
-   *  2. (Unix) Call `which opera` to find the location
-   *  3. Check if Opera exists at the default location on the respective OS
+   *  2. Check if Opera exists at the default location on the respective OS
+   *  3. (Unix) Call `which opera` to find the location
    *  4. Give up and return null
    * @return The path to Opera, or null
    */
 	public String operaPath() {
-		String path = null;
-		if ((path = checkPath(System.getenv("OPERA_PATH"))) != null)
-			return path;
+		String path = System.getenv("OPERA_PATH");
+
+		if(isPathValid(path)) return path;
+
 		Platform platform = Platform.getCurrent();
 
-		if (platform.is(Platform.LINUX) || platform.is(Platform.UNIX)) {
-			CommandLine line = new CommandLine("which", "opera");
-			line.execute();
-			path = line.getStdOut().trim();
-			if (checkPath(path) != null)
-				return path;
-		}
-
-		if (platform.is(Platform.LINUX) || platform.is(Platform.UNIX)) {
-			if ((path = checkPath("/usr/bin/opera")) != null)
-				return path;
-
-		} else if (platform.is(Platform.WINDOWS)) {
+		switch (platform) {
+		case LINUX:
+		case UNIX:
+			path = "/usr/bin/opera";
+			if (!isPathValid(path)) {
+				CommandLine line = new CommandLine("which", "opera");
+				line.execute();
+				path = line.getStdOut().trim();
+			}
+			break;
+		case MAC:
+			path = "/Applications/Opera.app/Contents/MacOS/Opera";
+			break;
+		case WINDOWS:
+		case XP:
+		case VISTA:
 			String x86 = System.getenv("ProgramFiles(x86)");
 			String progfiles = (x86 == null) ? System.getenv("PROGRAMFILES") : x86;
 			if (progfiles == null) progfiles = "\\Program Files";
-
-			if ((path = checkPath(progfiles + "\\Opera\\opera.exe")) != null)
-				return path;
-
-		} else if (platform.is(Platform.MAC)) {
-			if ((path = checkPath("/Applications/Opera.app/Contents/MacOS/Opera")) != null)
-				return path;
-
+			path = progfiles + "\\Opera\\opera.exe";
+			break;
+		default:
+			throw new WebDriverException("Auto find is not support on this platform"); //android?
 		}
 
-		return null;
+		return (isPathValid(path)) ? path : null;
 	}
+
 
   /**
    * This method will try and find Opera Launcher on any system.
    * @return The path to Opera Launcher, or null
    */
 	public String launcherPath() {
-		String path = null;
-		if ((path = checkPath(System.getenv("OPERA_LAUNCHER"))) != null)
-			return path;
+		String path = System.getenv("OPERA_LAUNCHER");
+		if (!isPathValid(path)) path = getOperaLauncherPath();
 
-		return getOperaLauncherPath();
+		return path;
 	}
 
 	/**
@@ -210,12 +210,12 @@ public class OperaPaths {
    * @return null if the path is null, or does not exist. Otherwise it returns
    * the given path.
    */
-  private String checkPath(String path) {
-    if (path == null) return null;
+  private boolean isPathValid(String path) {
+    if (path == null) return false;
+
+    if(path.length() == 0) return false;
 
     File file = new File(path);
-    if (file.exists()) return path;
-
-    return null;
+    return (file.exists());
   }
 }
