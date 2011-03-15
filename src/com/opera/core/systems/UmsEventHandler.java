2a7124e323112866f2329d7ed119b8710c281242
Stuart Knightley <stuartk@opera.com>
2011-03-15 14:17:19 +0100

    Un-deprecate UmsEventHandler
    Still used and no better replacement.

--- src/com/opera/core/systems/UmsEventHandler.java
+++ src/com/opera/core/systems/UmsEventHandler.java
@@ -22,11 +22,9 @@ import com.opera.core.systems.scope.protos.WmProtos.WindowInfo;
 /**
  * Provided support for an alternate protocol. To be removed.
  *
- * @deprecated
  * @author Deniz Turkoglu
  *
  */
-@Deprecated
 public class UmsEventHandler extends EventHandler {
 
 	public UmsEventHandler(ScopeServices services) {
@@ -35,21 +33,12 @@ public class UmsEventHandler extends EventHandler {
 
 	@Override
 	public void onRuntimeStarted(RuntimeInfo info) {
-		/*
-		if(info.getHtmlFramePath().equals("_top") && info.getWindowID() == services.getWindowManager().getActiveWindowId()) {
-			//check if we already have such a runtime and clean them up if needed
-			services.getDebugger().cleanUpRuntimes();
-			services.getDebugger().setRuntime(info);
-		}
-		*/
 		services.getDebugger().addRuntime(info);
 	}
 
 	@Override
 	public void onUpdatedWindow(WindowInfo info) {
-		//logger.log(Level.INFO, window.toString());
-		//if(info.getOpenerID() == 0)
-			services.getWindowManager().addWindow(info);
+		services.getWindowManager().addWindow(info);
 	}
 
 	@Override
