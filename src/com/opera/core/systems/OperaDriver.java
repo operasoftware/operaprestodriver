44f92676a0d83d140aaea04e42cdae8bc83ae09c
Stuart Knightley <stuartk@opera.com>
2011-06-21 12:18:01 +0200

    Make listPrefs return a map instead of a list.
    API change!

--- src/com/opera/core/systems/OperaDriver.java
+++ src/com/opera/core/systems/OperaDriver.java
@@ -1098,14 +1098,19 @@ public class OperaDriver implements WebDriver, FindsByLinkText, FindsById,
   }
 
   /**
-   * Returns a list of preferences in the requested section.
+   * Returns a Map of preference names to preferences in the requested section.
    *
    * @param sort Whether to alphabetically sort the preference keys.
    * @param section The section to retreive the preferences from.
-   * @return A list of preferences.
+   * @return A Map of preference names to preferences.
    */
-  public List<Pref> listPrefs(boolean sort, String section) {
-    return services.getPrefs().listPrefs(sort, section);
+  public Map<String, Pref> listPrefs(boolean sort, String section) {
+    Map<String, Pref> map = new HashMap<String, Pref>();
+
+    for (Pref p : prefs.listPrefs(sort, section)) {
+      map.put(p.getKey(), p);
+    }
+    return map;
   }
 
   /**
