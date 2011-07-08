80f2efb2a7413c338724d9cad68d0ea6fa35b8df
Arne Skjærholt <arnsholt@gmail.com>
2011-07-08 16:39:38 +0200

    Only run selftest tests on core-gogi product.

--- test/com/opera/core/systems/SelftestTest.java
+++ test/com/opera/core/systems/SelftestTest.java
@@ -12,7 +12,10 @@ import com.opera.core.systems.scope.services.ISelftest.SelftestResult;
 public class SelftestTest extends TestBase {
   @Test
   public void testSelftests() {
-    List<SelftestResult> results = driver.selftest(Arrays.asList("about"), 30000);
-    Assert.assertNotNull("Running selftests doesn't blow up, returns non-null result.", results);
+    String product = driver.getProduct();
+    if(product.equals("core-gogi")) {
+      List<SelftestResult> results = driver.selftest(Arrays.asList("about"), 30000);
+      Assert.assertNotNull("Running selftests doesn't blow up, returns non-null result.", results);
+    }
   }
 }
