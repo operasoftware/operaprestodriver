c71473bda78c8969937e660b5d9cd0b369be44cd
Andreas Tolf Tolfsen <andreastt@opera.com>
2011-08-31 13:59:37 +0200

    Reformatted OperaPaths and corrected some of the documentation

--- src/com/opera/core/systems/OperaPaths.java
+++ src/com/opera/core/systems/OperaPaths.java
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package com.opera.core.systems;
 
 import java.io.File;
@@ -22,6 +23,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.openqa.selenium.Platform;
@@ -34,57 +36,57 @@ import java.util.Arrays;
 import java.util.logging.Logger;
 
 /**
- * This class tries to find the paths to Opera and Opera Launcher on any system.
- * If it cannot find a launcher it will extract the appropriate one from the jar
- * it is in.
+ * This class tries to find the paths to Opera and Opera Launcher on any system. If it cannot find a
+ * launcher it will extract the appropriate one from the jar it is in.
  *
- * @author Stuart Knightley, Deniz Turkoglu
- *
+ * @author Stuart Knightley <stuartk@opera.com>, Deniz Turkoglu <dturkoglu@opera.com>
  */
 public class OperaPaths {
 
   private final Logger logger = Logger.getLogger(this.getClass().getName());
 
   /**
-   * This method will try and find Opera on any system. It takes the following
-   * steps:
-   *  1. Check the environment variable "OPERA_PATH". If it exists, and the
-   *     file it points to exists, then return
-   *  2. Check if Opera exists at the default location on the respective OS
-   *  3. (Unix) Call `which opera` to find the location
-   *  4. Give up and return null
-   * @return The path to Opera, or null
+   * This method will try and find Opera on any system. It takes the following steps: 1. Check the
+   * environment variable "OPERA_PATH". If it exists, and the file it points to exists, then return
+   * 2. Check if Opera exists at the default location on the respective OS 3. (Unix) Call `which
+   * opera` to find the location 4. Give up and return null
+   *
+   * @return the path to Opera, or null
    */
   public String operaPath() {
     String path = System.getenv("OPERA_PATH");
 
-    if (isPathValid(path)) return path;
+    if (isPathValid(path)) {
+      return path;
+    }
 
     Platform platform = Platform.getCurrent();
 
     switch (platform) {
-    case LINUX:
-    case UNIX:
-      path = "/usr/bin/opera";
-      if (!isPathValid(path)) {
-        CommandLine line = new CommandLine("which", "opera");
-        line.execute();
-        path = line.getStdOut().trim();
-      }
-      break;
-    case MAC:
-      path = "/Applications/Opera.app/Contents/MacOS/Opera";
-      break;
-    case WINDOWS:
-    case XP:
-    case VISTA:
-      String x86 = System.getenv("ProgramFiles(x86)");
-      String progfiles = (x86 == null) ? System.getenv("PROGRAMFILES") : x86;
-      if (progfiles == null) progfiles = "\\Program Files";
-      path = progfiles + "\\Opera\\opera.exe";
-      break;
-    default:
-      throw new WebDriverException("Auto find is not support on this platform"); // android?
+      case LINUX:
+      case UNIX:
+        path = "/usr/bin/opera";
+        if (!isPathValid(path)) {
+          CommandLine line = new CommandLine("which", "opera");
+          line.execute();
+          path = line.getStdOut().trim();
+        }
+        break;
+      case MAC:
+        path = "/Applications/Opera.app/Contents/MacOS/Opera";
+        break;
+      case WINDOWS:
+      case XP:
+      case VISTA:
+        String x86 = System.getenv("ProgramFiles(x86)");
+        String progfiles = (x86 == null) ? System.getenv("PROGRAMFILES") : x86;
+        if (progfiles == null) {
+          progfiles = "\\Program Files";
+        }
+        path = progfiles + "\\Opera\\opera.exe";
+        break;
+      default:
+        throw new WebDriverException("Auto find is not support on this platform"); // android?
     }
     return (isPathValid(path)) ? path : null;
   }
@@ -92,19 +94,20 @@ public class OperaPaths {
   /**
    * This method will try and find Opera Launcher on any system.
    *
-   * @return The path to Opera Launcher, or null
+   * @return the path to Opera Launcher, or null
    */
   public String launcherPath() {
     String path = System.getenv("OPERA_LAUNCHER");
-    if (!isPathValid(path)) path = getOperaLauncherPath();
+    if (!isPathValid(path)) {
+      path = getOperaLauncherPath();
+    }
 
     return path;
   }
 
   /**
-   * Tries to load the launcher executable from the jar file A copy of the
-   * launcher is put into user directory and on each call is compared to the
-   * content in jar file
+   * Tries to load the launcher executable from the jar file.  A copy of the launcher is put into
+   * user directory and on each call is compared to the content in jar file.
    *
    * @return path to launcher executable
    */
@@ -121,7 +124,7 @@ public class OperaPaths {
       // If the launcher is inside the jar we will need to copy it out
       if ((url.startsWith("jar:")) || (url.startsWith("wsjar:"))) {
         executablePath = FileUtils.getUserDirectoryPath() + File.separatorChar
-            + ".launcher" + File.separatorChar + launcherName;
+                         + ".launcher" + File.separatorChar + launcherName;
         File cur_launcher = new File(executablePath);
 
         // Whether we need to copy a new launcher across, either because
@@ -141,7 +144,9 @@ public class OperaPaths {
 
         if (copy == true) {
           try {
-            if (!cur_launcher.exists()) FileUtils.touch(cur_launcher);
+            if (!cur_launcher.exists()) {
+              FileUtils.touch(cur_launcher);
+            }
 
             InputStream is = res.openStream();
             OutputStream os = new FileOutputStream(cur_launcher);
@@ -154,7 +159,7 @@ public class OperaPaths {
             cur_launcher.setLastModified(cur_launcher.lastModified());
           } catch (IOException e) {
             throw new WebDriverException("Cant write file to disk : "
-                + e.getMessage());
+                                         + e.getMessage());
           }
           logger.fine("New launcher copied");
         }
@@ -171,15 +176,15 @@ public class OperaPaths {
       }
     }
 
-    if (executablePath != null) makeLauncherExecutable(executablePath);
+    if (executablePath != null) {
+      makeLauncherExecutable(executablePath);
+    }
 
     return executablePath;
   }
 
   /**
-   * Chmods the file at given path (linux and mac only)
-   *
-   * @param executablePath
+   * Makes the launcher executable by chmod'ing the file at given path (GNU/Linux and Mac only).
    */
   private void makeLauncherExecutable(String executablePath) {
     Platform current = Platform.getCurrent();
@@ -189,37 +194,46 @@ public class OperaPaths {
     }
   }
 
+  /**
+   * Get the launcher's binary file name based on what flavour of operating system and what kind of
+   * architecture the user is using.
+   *
+   * @return the launcher's binary file name
+   */
   private String getLauncherNameForOS() {
     boolean is64 = "64".equals(System.getProperty("sun.arch.data.model"));
     Platform currentPlatform = Platform.getCurrent();
 
     switch (currentPlatform) {
-    case LINUX:
-    case UNIX:
-      return (is64 ? "launcher-linux-x86_64" : "launcher-linux-i686");
-    case MAC:
-      return "launcher-mac";
-    case WINDOWS:
-    case VISTA:
-    case XP:
-      return "launcher-win32-i86pc.exe";
-    default:
-      throw new WebDriverException(
-          "Could not find a platform that supports bundled launchers, please set it manually");
+      case LINUX:
+      case UNIX:
+        return (is64 ? "launcher-linux-x86_64" : "launcher-linux-i686");
+      case MAC:
+        return "launcher-mac";
+      case WINDOWS:
+      case VISTA:
+      case XP:
+        return "launcher-win32-i86pc.exe";
+      default:
+        throw new WebDriverException(
+            "Could not find a platform that supports bundled launchers, please set it manually");
     }
   }
 
   /**
    * Check that the given path is not null, and exists.
    *
-   * @param path The path to check.
-   * @return null if the path is null, or does not exist. Otherwise it returns
-   *         the given path.
+   * @param path the path to check
+   * @return true if the path is valid, false otherwise
    */
   private boolean isPathValid(String path) {
-    if (path == null) return false;
+    if (path == null) {
+      return false;
+    }
 
-    if (path.length() == 0) return false;
+    if (path.length() == 0) {
+      return false;
+    }
 
     File file = new File(path);
     return (file.exists());
@@ -228,13 +242,13 @@ public class OperaPaths {
   /**
    * Get the MD5 hash of the given stream.
    *
-   * @param fis The input stream to use
-   * @return A byte array of the MD5 hash
-   * @throws Exception
+   * @param fis the input stream to use
+   * @return a byte array of the MD5 hash
    */
   private static byte[] md5(InputStream fis) throws Exception {
     byte[] buffer = new byte[1024];
     MessageDigest complete = MessageDigest.getInstance("MD5");
+
     int numRead;
     do {
       numRead = fis.read(buffer);
@@ -243,9 +257,16 @@ public class OperaPaths {
       }
     } while (numRead != -1);
     fis.close();
+
     return complete.digest();
   }
 
+  /**
+   * Get the MD5 hash of the given file path.
+   *
+   * @param filename the path to the file
+   * @return a byte array of the MD5 hash
+   */
   private static byte[] md5(String filename) throws Exception {
     return md5(new FileInputStream(filename));
   }
