97f5afc1b350af1b25d00647d7f1712892990fad
Andreas Tolf Tolfsen <andreastt@opera.com>
2011-09-06 12:37:48 +0200

    Reformatted OperaLauncherProtocol

--- src/com/opera/core/systems/runner/launcher/OperaLauncherProtocol.java
+++ src/com/opera/core/systems/runner/launcher/OperaLauncherProtocol.java
@@ -13,27 +13,30 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package com.opera.core.systems.runner.launcher;
 
 import com.google.protobuf.GeneratedMessage;
 import com.google.protobuf.InvalidProtocolBufferException;
+
 import com.opera.core.systems.runner.launcher.OperaLauncherProtos.LauncherHandshakeResponse;
 import com.opera.core.systems.runner.launcher.OperaLauncherProtos.LauncherScreenshotResponse;
 import com.opera.core.systems.runner.launcher.OperaLauncherProtos.LauncherStatusResponse;
+
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketException;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.EnumSet;
-import java.util.logging.Logger;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.EnumSet;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Logger;
 
 /**
  * Implements the Opera launcher protocol.
  *
- * @author Jan Vidar Krey (janv@opera.com)
+ * @author Jan Vidar Krey <janv@opera.com>
  */
 public class OperaLauncherProtocol {
 
@@ -43,6 +46,7 @@ public class OperaLauncherProtocol {
   public enum MessageType {
     MSG_HELLO((byte) 0), MSG_START((byte) 1), MSG_STATUS((byte) 2), MSG_SCREENSHOT(
         (byte) 3), MSG_STOP((byte) 4), MSG_SHUTDOWN((byte) 5);
+
     MessageType(byte n) {
       code = n;
     }
@@ -50,8 +54,9 @@ public class OperaLauncherProtocol {
     private static final Map<Byte, MessageType> lookup = new TreeMap<Byte, MessageType>();
 
     static {
-      for (MessageType command : EnumSet.allOf(MessageType.class))
+      for (MessageType command : EnumSet.allOf(MessageType.class)) {
         lookup.put(command.getValue(), command);
+      }
     }
 
     private byte code;
@@ -67,6 +72,7 @@ public class OperaLauncherProtocol {
   }
 
   public class ResponseEncapsulation {
+
     private boolean success;
     private GeneratedMessage response;
 
@@ -90,7 +96,7 @@ public class OperaLauncherProtocol {
     this.socket.setSoTimeout(180000);
     logger.fine("TIMEOUT=" + this.socket.getSoTimeout());
     logger.fine("Got Opera launcher connection from "
-        + socket.getRemoteSocketAddress().toString());
+                + socket.getRemoteSocketAddress().toString());
   }
 
   public void shutdown() throws IOException {
@@ -114,7 +120,7 @@ public class OperaLauncherProtocol {
     buf.putInt(size);
     buf.flip();
     logger.finest("SEND: type=" + (0) + ", command=" + ((int) type.getValue())
-        + ", size=" + size);
+                  + ", size=" + size);
     socket.getOutputStream().write(buf.array());
   }
 
@@ -124,13 +130,14 @@ public class OperaLauncherProtocol {
    * @param type The request type to be sent
    * @param body The serialized request payload
    * @return The response
-   * @throws java.io.IOException
    */
   public ResponseEncapsulation sendRequest(MessageType type, byte[] body)
       throws IOException {
     int size = (body != null) ? size = body.length : 0;
     sendRequestHeader(type, size);
-    if (body != null) socket.getOutputStream().write(body);
+    if (body != null) {
+      socket.getOutputStream().write(body);
+    }
     return recvMessage();
   }
 
@@ -139,13 +146,14 @@ public class OperaLauncherProtocol {
    *
    * @param type The request type to be sent
    * @param body The serialized request payload
-   * @throws java.io.IOException
    */
   public void sendRequestWithoutResponse(MessageType type, byte[] body)
       throws IOException {
     int size = (body != null) ? size = body.length : 0;
     sendRequestHeader(type, size);
-    if (body != null) socket.getOutputStream().write(body);
+    if (body != null) {
+      socket.getOutputStream().write(body);
+    }
   }
 
   /**
@@ -178,8 +186,10 @@ public class OperaLauncherProtocol {
     byte[] headers = new byte[8];
     recv(headers, headers.length);
 
-    if (headers[0] != 'L' || headers[1] != '1') throw new IOException(
-        "Wrong launcher protocol header");
+    if (headers[0] != 'L' || headers[1] != '1') {
+      throw new IOException(
+          "Wrong launcher protocol header");
+    }
 
     ByteBuffer buf = ByteBuffer.allocate(4);
     buf.order(ByteOrder.BIG_ENDIAN);
@@ -189,7 +199,7 @@ public class OperaLauncherProtocol {
     int size = buf.getInt();
 
     logger.finest("RECV: type=" + ((int) headers[3]) + ", command="
-        + ((int) headers[2]) + ", size=" + size);
+                  + ((int) headers[2]) + ", size=" + size);
 
     byte[] data = new byte[size];
     recv(data, size);
@@ -197,34 +207,38 @@ public class OperaLauncherProtocol {
     boolean success = (headers[3] == (byte) 1);
     MessageType type = MessageType.get(headers[2]);
 
-    if (type == null) throw new IOException("Unable to determine message type");
+    if (type == null) {
+      throw new IOException("Unable to determine message type");
+    }
 
-    if ((headers[3] != (byte) 1) && (headers[3] != (byte) 2)) throw new IOException(
-        "Unable to determine success or error");
+    if ((headers[3] != (byte) 1) && (headers[3] != (byte) 2)) {
+      throw new IOException(
+          "Unable to determine success or error");
+    }
 
     switch (type) {
-    case MSG_HELLO: {
-      LauncherHandshakeResponse.Builder response = LauncherHandshakeResponse.newBuilder();
-      buildMessage(response, data);
-      msg = response.build();
-      break;
-    }
+      case MSG_HELLO: {
+        LauncherHandshakeResponse.Builder response = LauncherHandshakeResponse.newBuilder();
+        buildMessage(response, data);
+        msg = response.build();
+        break;
+      }
 
-    case MSG_START:
-    case MSG_STATUS:
-    case MSG_STOP: {
-      LauncherStatusResponse.Builder response = LauncherStatusResponse.newBuilder();
-      buildMessage(response, data);
-      msg = response.build();
-      break;
-    }
+      case MSG_START:
+      case MSG_STATUS:
+      case MSG_STOP: {
+        LauncherStatusResponse.Builder response = LauncherStatusResponse.newBuilder();
+        buildMessage(response, data);
+        msg = response.build();
+        break;
+      }
 
-    case MSG_SCREENSHOT: {
-      LauncherScreenshotResponse.Builder response = LauncherScreenshotResponse.newBuilder();
-      buildMessage(response, data);
-      msg = response.build();
-      break;
-    }
+      case MSG_SCREENSHOT: {
+        LauncherScreenshotResponse.Builder response = LauncherScreenshotResponse.newBuilder();
+        buildMessage(response, data);
+        msg = response.build();
+        break;
+      }
 
     }
 
@@ -237,8 +251,8 @@ public class OperaLauncherProtocol {
       return builder.mergeFrom(message);
     } catch (InvalidProtocolBufferException ex) {
       throw new IOException("Could not parse launcher message "
-          + builder.getDescriptorForType().getFullName() + " : "
-          + ex.getMessage());
+                            + builder.getDescriptorForType().getFullName() + " : "
+                            + ex.getMessage());
     }
   }
 
