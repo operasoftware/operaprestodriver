90f5d3bab714070d3ce62ca316686a8e9f1e3c40
Stuart Knightley <stuartk@opera.com>
2011-09-07 16:45:46 +0200

    Give OperaWebElement its own logger
    Fix spelling mistake in comment.

--- src/com/opera/core/systems/OperaWebElement.java
+++ src/com/opera/core/systems/OperaWebElement.java
@@ -20,6 +20,7 @@ import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
@@ -59,6 +60,8 @@ public class OperaWebElement extends RemoteWebElement {
   private final int runtimeId;
 
   private final IOperaExec execService;
+
+  protected final Logger logger = Logger.getLogger(this.getClass().getName());
 
   public int getObjectId() {
     return objectId;
@@ -548,13 +551,13 @@ public class OperaWebElement extends RemoteWebElement {
         objectId
     );
 
-    // FIXME: the goog.dom.getDocumentScrollElement_() functionin the Google
+    // FIXME: the goog.dom.getDocumentScrollElement_() function the Google
     // closure library doesn't return the document for SVG documents. This
     // is used by the above atom. In this case the coordinates string will be
     // empty, so we use this fallback to get the coordinates. Hopefully a fix
     // will be forthcoming in the closure library.
     if (coordinates.isEmpty()) {
-      parent.logger.warning("Falling back to non-atom positioning code in getLocation");
+      logger.warning("Falling back to non-atom positioning code in getLocation");
       coordinates = debugger.callFunctionOnObject(
           "var coords = locator.getBoundingClientRect();" +
           "return (coords.left-window.pageXOffset)+','+(coords.top-window.pageYOffset)",
