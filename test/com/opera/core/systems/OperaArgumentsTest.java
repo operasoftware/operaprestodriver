5a4eb016d9de7e28d6c1bda5a8a7d89731ce9c5d
Andreas Tolf Tolfsen <andreastt@opera.com>
2011-11-02 10:45:19 +0100

    Importing static assertion methods from JUnit and ignoring two tests due to the bug in core-gogi with -pd

--- test/com/opera/core/systems/OperaArgumentsTest.java
+++ test/com/opera/core/systems/OperaArgumentsTest.java
@@ -1,76 +1,91 @@
+/*
+Copyright 2011 Opera Software ASA
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package com.opera.core.systems;
 
 import com.opera.core.systems.arguments.OperaArgument;
 import com.opera.core.systems.arguments.OperaCoreArguments;
 import com.opera.core.systems.arguments.OperaDesktopArguments;
 
-import junit.framework.Assert;
-
-import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 public class OperaArgumentsTest {
 
   private static OperaArguments arguments;
 
-   @Test
+  @Test
   public void testConstructingOperaArguments() {
     arguments = new OperaArguments();
-    Assert.assertNotNull(arguments);
+    assertNotNull(arguments);
   }
 
   @Test
   public void testAddArgumentWithKey() {
     arguments.add("foo");
-    Assert.assertNotNull(arguments.getArguments());
-    Assert.assertEquals(1, arguments.getArguments().size());
+    assertNotNull(arguments.getArguments());
+    assertEquals(1, arguments.size());
   }
 
   @Test
   public void testAddArgumentWithKeyAndValue() {
     arguments.add("foo", "bar");
-    Assert.assertEquals("foo", arguments.getArguments().get(1).getArgument());
-    Assert.assertEquals("bar", arguments.getArguments().get(1).getValue());
+    assertEquals("foo", arguments.get(1).getArgument());
+    assertEquals("bar", arguments.get(1).getValue());
   }
 
   @Test
   public void testAddArgumentWithOperaArgument() {
     OperaArgument argument = new OperaArgument("hooba", "flooba");
     arguments.add(argument);
-    Assert.assertEquals("hooba", arguments.getArguments().get(2).getArgument());
-    Assert.assertEquals("flooba", arguments.getArguments().get(2).getValue());
+    assertEquals("hooba", arguments.get(2).getArgument());
+    assertEquals("flooba", arguments.get(2).getValue());
   }
 
   @Test
   public void testGettingArgumentsAsObjectList() {
-    Assert.assertEquals(3, arguments.getArguments().size());
+    assertEquals(3, arguments.size());
   }
 
   @Test
   public void testGettingArgumentsAsStringList() {
-    Assert.assertEquals(5, arguments.getArgumentsAsStringList().size());
-    Assert.assertEquals("hooba", arguments.getArgumentsAsStringList().get(3));
+    assertEquals(5, arguments.getArgumentsAsStringList().size());
+    assertEquals("hooba", arguments.getArgumentsAsStringList().get(3));
   }
 
   @Test
   public void testParsingString() {
     OperaArguments parsed = OperaArguments.parse("-foo -bar baz -bah=abc");
-    Assert.assertEquals("foo", parsed.getArguments().get(0).getArgument());
-    Assert.assertEquals("bar", parsed.getArguments().get(1).getArgument());
-    Assert.assertEquals("baz", parsed.getArguments().get(1).getValue());
-    Assert.assertEquals("bah", parsed.getArguments().get(2).getArgument());
+    assertEquals("foo", parsed.get(0).getArgument());
+    assertEquals("bar", parsed.get(1).getArgument());
+    assertEquals("baz", parsed.get(1).getValue());
+    assertEquals("bah", parsed.get(2).getArgument());
   }
 
   @Test
   public void testParsingStringWithArgumentSign() {
     OperaArguments parsed = OperaArguments.parse("--foo --bar baz --bah=abc");
-    Assert.assertEquals("foo", parsed.getArguments().get(0).getArgument());
-    Assert.assertEquals("bar", parsed.getArguments().get(1).getArgument());
-    Assert.assertEquals("baz", parsed.getArguments().get(1).getValue());
-    Assert.assertEquals("bah", parsed.getArguments().get(2).getArgument());
+    assertEquals("foo", parsed.get(0).getArgument());
+    assertEquals("bar", parsed.get(1).getArgument());
+    assertEquals("baz", parsed.get(1).getValue());
+    assertEquals("bah", parsed.get(2).getArgument());
   }
 
   @Test
@@ -79,29 +94,29 @@ public class OperaArgumentsTest {
     extraArguments.add("abc", "def");
     extraArguments.add("ghi");
     arguments.merge(extraArguments);
-    Assert.assertEquals("foo", arguments.getArguments().get(0).getArgument());
-    Assert.assertEquals("abc", arguments.getArguments().get(3).getArgument());
-    Assert.assertEquals("def", arguments.getArguments().get(3).getValue());
-    Assert.assertEquals("ghi", arguments.getArguments().get(4).getArgument());
-    Assert.assertNull(arguments.getArguments().get(4).getValue());
+    assertEquals("foo", arguments.get(0).getArgument());
+    assertEquals("abc", arguments.get(3).getArgument());
+    assertEquals("def", arguments.get(3).getValue());
+    assertEquals("ghi", arguments.get(4).getArgument());
+    assertNull(arguments.get(4).getValue());
   }
-
 
   // Extensions follow.
 
-
   @Test
+  @Ignore("Currently disabled due to bug in core-gogi")
   public void testConstructingOperaCoreArguments() {
     OperaArguments arguments = new OperaCoreArguments();
-    Assert.assertNotNull(arguments);
-    Assert.assertEquals("autotestmode", arguments.getArguments().get(0).getArgument());
+    assertNotNull(arguments);
+    assertEquals("autotestmode", arguments.get(0).getArgument());
   }
 
   @Test
+  @Ignore("Currently disabled due to bug in OperaCoreArguments, which this derives off")
   public void testConstructingOperaDesktopArguments() {
     OperaArguments arguments = new OperaDesktopArguments();
-    Assert.assertNotNull(arguments);
-    Assert.assertEquals("autotestmode", arguments.getArguments().get(0).getArgument());
+    assertNotNull(arguments);
+    assertEquals("autotestmode", arguments.get(0).getArgument());
   }
 
 }
