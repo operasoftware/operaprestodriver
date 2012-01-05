6f5185104cf0781fb366b804723ae56710cea8c4
Michal Zajaczkowski <mzajaczkowski@opera.com>
2012-01-05 10:11:44 +0100

    DSK-354630 Webdriver QuickMenu doesn't have an isVisible() method, that is expected by operawatir

--- src/com/opera/core/systems/QuickMenu.java
+++ src/com/opera/core/systems/QuickMenu.java
@@ -14,7 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package com.opera.core.systems;
+import java.util.List;
+import java.util.ArrayList;
 
 import com.opera.core.systems.scope.protos.DesktopWmProtos.DesktopWindowRect;
 import com.opera.core.systems.scope.protos.DesktopWmProtos.QuickMenuInfo;
@@ -31,6 +32,104 @@ import java.util.List;
  * @author Karianne Ekern
  */
 public class QuickMenu {
+		private final QuickMenuInfo info;
+		private final IDesktopUtils desktopUtils;
+		private final SystemInputManager systemInputManager;
+
+		/**
+		 * Constructor.
+		 *
+		 * @param desktopUtils
+		 * @param inputManager
+		 * @param info QuickMenuInfo of the menu
+		 */
+		public QuickMenu(QuickMenuInfo info, IDesktopUtils desktopUtils, SystemInputManager inputManager) {
+	        this.info = info;
+	        this.desktopUtils = desktopUtils;
+	        this.systemInputManager = inputManager;
+	    }
+
+		/**
+	     * Get the name of the menu (as represented in e.g. standard_menu.ini)
+	     *
+	     * @return the name of the menu
+	     */
+		public String getName() {
+			return info.getMenuId().getMenuName();
+		}
+
+		/**
+		 * It's assumed here that a menu won't be passed to Watir if it is not visible.
+		 * This is probably not entirely true and probably will need changing.
+		 *
+		 * @return Always returns true.
+		 */
+		public boolean isVisible() {
+			return true;
+		}
+
+		/**
+		 * Get the windowId of the window the Menu is attached to.
+		 * Note: Only set for menubars, and only on windows and *nix.
+		 * 
+		 * @return window id 
+		 */
+		public int getParentWindowId() {
+			return info.getWindowId().getWindowID();
+		}
+
+		/**
+		 * 
+		 * @return List of QuickMenuItems in this menu
+		 */
+		public List<QuickMenuItem> getItemList() {
+			List<QuickMenuItem> itemList = new ArrayList<QuickMenuItem>();
+			for (QuickMenuItemInfo itemInfo : info.getMenuItemListList()) {
+				QuickMenuItem item = new QuickMenuItem(itemInfo, getName(), desktopUtils, systemInputManager);
+				
+				itemList.add(item);
+			}
+			return itemList;
+		}
+
+		/**
+		 * 
+		 * @return true if this menu has one or more submenus, else false
+		 */
+		public boolean hasSubmenu() {
+			// Checks items in menu for submenu, return true
+			// on first true else false if none.
+			List<QuickMenuItem> items = getItemList();
+			for (QuickMenuItem item : items) {
+				if (item.hasSubMenu())
+					return true;
+			}
+			return false;
+		}
+		
+
+		/**
+		 * @return DesktopWindowRect of the menu
+		 */
+		public DesktopWindowRect getRect() {
+			return info.getRect();
+		}
+		
+		@Override
+		public String toString() {
+			return "QuickMenu " + getName();
+		}
+
+		public String toFullString() {
+			return "QuickMenu\n" +
+			  "       Menu name: " + getName() + "\n"
+			+ "          x: " + getRect().getX() + "\n"
+			+ "          y: " + getRect().getY() + "\n"
+			+ "      width: " + getRect().getWidth() + "\n"
+			+ "     height: " + getRect().getHeight() + " \n";
+		}
+
+}
 
   private final QuickMenuInfo info;
   private final IDesktopUtils desktopUtils;
