e8dd076540bf67e0f842435f3c0d50ffff11cea7
Richard Atkins <rjatkins@optusnet.com.au>
2012-01-22 21:48:33 +1100

    Added a fix (and corresponding test) for WebElements being proxied by PageFactory

---
+++ test/com/opera/core/systems/IFramesTest.java
@@ -0,0 +1,40 @@
+/*
+Copyright 2011-2012 Opera Software ASA
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package com.opera.core.systems;
+
+import com.opera.core.systems.pages.IFramePage;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.support.PageFactory;
+
+import static org.junit.Assert.assertNotNull;
+
+public class IFramesTest extends OperaDriverTestCase {
+  IFramePage iFramePage;
+  @Before
+  public void bindPage() {
+    getFixture("iframes.html");
+    iFramePage = PageFactory.initElements(driver, IFramePage.class);
+    iFramePage.driver = driver;
+  }
+
+  @Test
+  public void testIFrameFocus() {
+    assertNotNull(iFramePage.getIFrame());
+    iFramePage.leaveIFrame();
+  }
+}
