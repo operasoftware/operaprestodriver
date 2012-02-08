39a792cf9c631c4bce57ccdcab4f3dcdbac6deca
Andreas Tolf Tolfsen <andreastt@opera.com>
2012-02-08 23:11:09 +0100

    Adding pseudo code for NoDriver annotation

---
+++ test/com/opera/core/systems/testing/NoDriver.java
@@ -0,0 +1,12 @@
+package com.opera.core.systems.testing;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface NoDriver {
+
+}
