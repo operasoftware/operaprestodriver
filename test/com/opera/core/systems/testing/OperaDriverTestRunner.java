307d136f7ee547f909961436fa7dbcafd4ee5372
Andreas Tolf Tolfsen <andreastt@opera.com>
2012-02-22 17:40:03 +0100

    Simplified ignore annotation by defaulting to no products/platforms in Ignore
    While we typically would want to specify the default to be any product
    or any platform, we need to perform two checks for this annotation, both
    of which are mutually exclusive.
    
    This means that if one of the checks for platform or product is true,
    the other one will be skipped.  If we check for ANY product, it will
    always find something to match.

--- test/com/opera/core/systems/testing/OperaDriverTestRunner.java
+++ test/com/opera/core/systems/testing/OperaDriverTestRunner.java
@@ -17,8 +17,6 @@ limitations under the License.
 package com.opera.core.systems.testing;
 
 import com.opera.core.systems.OperaProduct;
-import com.opera.core.systems.testing.Ignore;
-import com.opera.core.systems.testing.OperaDriverTestCase;
 import com.opera.core.systems.testing.drivers.OperaDriverBuilder;
 import com.opera.core.systems.testing.drivers.TestOperaDriver;
 import com.opera.core.systems.testing.drivers.TestOperaDriverSupplier;
@@ -26,7 +24,6 @@ import com.opera.core.systems.testing.drivers.TestOperaDriverSupplier;
 import org.junit.internal.AssumptionViolatedException;
 import org.junit.internal.runners.model.EachTestNotifier;
 import org.junit.runner.Description;
-import org.junit.runner.RunWith;
 import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.FrameworkMethod;
@@ -39,18 +36,18 @@ import static com.opera.core.systems.testing.OperaDriverTestCase.driver;
  * The OperaDriverTestRunner can be used, amongst other things, for applying advanced ignores to
  * individual test cases (methods) or test suits (classes).
  *
- * Before a test run it will launch the default Opera using {@link com.opera.core.systems.OperaDriver} to determine the
- * current product's name.  You can then use <code>@Ignore(product = Product.CORE_SMARTPHONE)</code>
- * in your tests to ignore a certain product.  See {@link com.opera.core.systems.OperaProduct} for a full list of
- * products.
+ * Before a test run it will launch the default Opera using {@link com.opera.core.systems.OperaDriver}
+ * to determine the current product's name.  You can then use <code>@Ignore(product =
+ * Product.CORE_SMARTPHONE)</code> in your tests to ignore a certain product.  See {@link
+ * com.opera.core.systems.OperaProduct} for a full list of products.
  *
  * You may also utilize Selenium's {@link Platform} enum to ignore platforms/operating systems.  To
  * combine this with ignoring a product, an ignore rule (annotation) might look like <code>@Ignore({
  * product = Product.CORE_SMARTPHONE, platform = Platform.LINUX })</code>.
  *
  * You can specify the current product manually by setting the <code>OPERA_PRODUCT</code>
- * environment variable to a value defined in {@link com.opera.core.systems.OperaProduct}.  This will skip the step of
- * creating a browser instance for determining the current product.
+ * environment variable to a value defined in {@link com.opera.core.systems.OperaProduct}.  This
+ * will skip the step of creating a browser instance for determining the current product.
  *
  * To use this class, apply the <code>@RunWith(OperaDriverTestRunner.class)</code> annotation to
  * your class, or extend the {@link OperaDriverTestCase} class.
@@ -59,7 +56,8 @@ import static com.opera.core.systems.testing.OperaDriverTestCase.driver;
  */
 public class OperaDriverTestRunner extends BlockJUnit4ClassRunner {
 
-  public OperaDriverTestRunner(Class<? extends OperaDriverTestCase> klass) throws InitializationError {
+  public OperaDriverTestRunner(Class<? extends OperaDriverTestCase> klass)
+      throws InitializationError {
     super(klass);
   }
 
@@ -94,51 +92,30 @@ public class OperaDriverTestRunner extends BlockJUnit4ClassRunner {
    * check for this is mutually exclusive, meaning that if <em>either</em> the product or the
    * platform is true, the test will be ignored.
    *
-   * @param ignoreAnnotation a custom ignore annotation
+   * @param ignore a custom ignore annotation
    * @return true if test should be ignored, false otherwise
    */
-  private boolean shouldIgnore(Ignore ignoreAnnotation) {
-    if (ignoreAnnotation == null) {
+  private boolean shouldIgnore(Ignore ignore) {
+    if (ignore == null) {
       return false;
     }
 
-    // If it's a plain old @Ignore without arguments
-    if (isPlainIgnore(ignoreAnnotation)) {
-      return true;
-    }
+    OperaProduct currentProduct = OperaDriverTestCase.driver.utils().getProduct();
+    Platform currentPlatform = OperaDriverTestCase.driver.utils().getPlatform();
 
-    for (OperaProduct product : ignoreAnnotation.products()) {
-      if (product.is(OperaProduct.ALL)) {
-        break;
-      } else if (product.is(OperaDriverTestCase.driver.utils().getProduct())) {
+    for (OperaProduct product : ignore.products()) {
+      if (product.is(currentProduct)) {
         return true;
       }
     }
 
-    for (Platform platform : ignoreAnnotation.platforms()) {
-      if (platform.is(Platform.ANY)) {
-        // While ANY really should ignore any platform, ANY is also the default value for our custom
-        // Ignore annotation, meaning it will ignore everything by default.  So this is an exception
-        // from the rule.
-        return false;
-      } else if (platform.is(OperaDriverTestCase.driver.utils().getPlatform())) {
+    for (Platform platform : ignore.platforms()) {
+      if (platform.is(currentPlatform)) {
         return true;
       }
     }
 
-    // Should not be ignored, none of the rules apply
     return false;
-  }
-
-  /**
-   * Checks if provided ignore annotation is a plain old @Ignore without arguments.
-   *
-   * @param annotation the annotation to check
-   * @return true if annotation is without arguments, false otherwise
-   */
-  private boolean isPlainIgnore(Ignore annotation) {
-    return (annotation.products().length == 1 && annotation.products()[0].is(OperaProduct.ALL)) &&
-        (annotation.platforms().length == 1 && annotation.platforms()[0].is(Platform.ANY));
   }
 
   // copy of BlockJUnit4ClassRunner.runNotIgnored()
