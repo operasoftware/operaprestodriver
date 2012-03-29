591bcba0c0403353d29523a98d89e25c961a79b5
Andreas Tolf Tolfsen <andreastt@opera.com>
2012-03-29 11:09:00 +0200

    Restored valid tests for OperaPaths

--- test/com/opera/core/systems/OperaPathsTest.java
+++ test/com/opera/core/systems/OperaPathsTest.java
@@ -14,187 +14,37 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-/*
 package com.opera.core.systems;
 
-import junit.framework.Assert;
-
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.Platform;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.io.FileHandler;
 
 import java.io.File;
-import java.lang.reflect.Field;
-import java.util.Collections;
-import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 public class OperaPathsTest {
 
-  private static String oldOperaPath;
-  private static OperaPaths paths = new OperaPaths();
-  private static String knownDir = System.getProperty("user.home");
+  // TODO(andreastt): Abstract operaPath() so that we can provide OS to it for more testing
 
-  @BeforeClass
-  public static void beforeAll() {
-    if (oldOperaPath == null) {
-      String path = System.getenv("OPERA_PATH");
-      oldOperaPath = (path != null) ? path : "";
-    }
-  }
-
-  @Before
-  public void beforeEach() throws Exception {
-    if (!Platform.getCurrent().is(Platform.WINDOWS)) {
-      setEnvVar("OPERA_PATH", "");
-      setEnvVar("OPERA_LAUNCHER", "");
-    }
+  @Test
+  public void operaPathIsNotNull() {
+    assertNotNull(OperaPaths.operaPath());
   }
 
   @Test
-  public void testOperaPath() throws Exception {
-    assertNotNull(paths.operaPath());
+  public void operaPathExists() {
+    assertTrue(new File(OperaPaths.operaPath()).exists());
   }
 
   @Test
-  public void testLauncherPath() throws Exception {
-    assertNotNull(paths.launcherPath());
+  public void operaPathIsExecutable() {
+    assertTrue(FileHandler.canExecute(new File(OperaPaths.operaPath())));
   }
 
-  /**
-   * Test that the launcher is extracted correctly.
-   *
-  @Test
-  public void testNoLauncher() throws Exception {
-    File launcher = new File(paths.launcherPath());
-    launcher.delete();
+  // TODO(andreastt): Add tests for environmental variables in operaPath()
 
-    assertFalse(launcher.exists());
-    paths.launcherPath();
-    assertTrue(launcher.exists());
-  }
-
-  /**
-   * Test that the launcher is re-extracted when the hashes differ.
-   *
-  @Test
-  public void testNoMatch() throws Exception {
-    File launcher = new File(paths.launcherPath());
-    launcher.delete();
-
-    assertFalse(launcher.exists());
-    launcher.createNewFile();
-    assertEquals(0, launcher.length());
-
-    paths.launcherPath();
-
-    Assert.assertNotSame(0, launcher.length());
-  }
-
-  /**
-   * Test that the launcher isn't extracted when it is the same.
-   *
-  @Test
-  public void testExists() throws Exception {
-    File launcher = new File(paths.launcherPath());
-    long modified = launcher.lastModified();
-
-    paths.launcherPath();
-    assertEquals(modified, launcher.lastModified());
-  }
-
-  /**
-   * Test that Opera is started.  We're restoring the old OPERA_PATH here, although it might not be
-   * set.  This could potentially break on some non-standard sytems.
-   *
-  @Test
-  public void testStart() throws Exception {
-    setEnvVar("OPERA_PATH", oldOperaPath);
-
-    OperaDriver driver = new OperaDriver();
-    driver.navigate().to("opera:config");
-    assertEquals("opera:config", driver.getCurrentUrl());
-    driver.quit();
-  }
-
-  // These tests mess with the environment variables.  So they're at the end.
-
-  @Test
-  public void testOperaEnvVar() throws Exception {
-    assertNotSame(knownDir, paths.operaPath());
-    setEnvVar("OPERA_PATH", knownDir);
-    assertEquals(knownDir, paths.operaPath());
-  }
-
-  @Test
-  public void testInvalidOperaEnvVar() throws Exception {
-    setEnvVar("OPERA_PATH", "/invalid/path");
-
-    try {
-      WebDriver driver = new OperaDriver();
-    } catch (WebDriverException e) {
-      assertTrue("'" + e.getMessage() + "' contains 'does not exist'",
-                 e.getMessage().contains("does not exist"));
-      return;
-    }
-
-    fail();
-  }
-
-  @Test
-  public void testLauncherEnvVar() throws Exception {
-    assertNotSame(knownDir, paths.launcherPath());
-    setEnvVar("OPERA_LAUNCHER", knownDir);
-    assertEquals(knownDir, paths.launcherPath());
-  }
-
-  @Test
-  public void testInvalidLauncherEnvVar() throws Exception {
-    setEnvVar("OPERA_LAUNCHER", "/invalid/path");
-
-    try {
-      WebDriver driver = new OperaDriver();
-    } catch (WebDriverException e) {
-      assertTrue("'" + e.getMessage() + "' contains 'does not exist'",
-                 e.getMessage().contains("does not exist"));
-      return;
-    }
-
-    fail();
-  }
-
-  /**
-   * Massive hack to set the environment variables inside this JVM.  Used to test if OperaPaths is
-   * checking the environment variables.
-   *
-   * http://stackoverflow.com/questions/318239/how-do-i-set-environment-variables-from-java/496849#496849
-   *
-   * @param key   the new environment variable's identifier
-   * @param value the new environment variable's value
-   *
-  @SuppressWarnings("unchecked")
-  private static void setEnvVar(String key, String value) throws Exception {
-    Class<?>[] classes = Collections.class.getDeclaredClasses();
-    Map<String, String> env = System.getenv();
-    for (Class<?> cl : classes) {
-      if ("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
-        Field field = cl.getDeclaredField("m");
-        field.setAccessible(true);
-        Object obj = field.get(env);
-        Map<String, String> map = (Map<String, String>) obj;
-        map.put(key, value);
-      }
-    }
-  }
+  // TODO(andreastt): Add tests for OperaPaths.isPathValid()
 
 }
-   */
