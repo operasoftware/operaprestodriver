d0888a2b982a9c5952e8035de5a2d24bf0a62ef6
Michal Zajaczkowski <mzajaczkowski@opera.com>
2012-05-17 15:37:15 +0200

    Don't use the hacky environment variable override method, it causes nothing but trouble on Mac.

--- test/com/opera/core/systems/OperaDesktopDriverTestCase.java
+++ test/com/opera/core/systems/OperaDesktopDriverTestCase.java
@@ -79,7 +79,7 @@ public abstract class OperaDesktopDriverTestCase {
   public String setOperaPath() {
     String binaryPath = OperaPaths.findOperaInstallationPath();
     assertNotNull("Could not find an Opera installation on this machine.", binaryPath);
-    setEnvVariable("OPERA_PATH", binaryPath);
+    OperaPaths.overrideOperaPathEnvVar(binaryPath);
     return binaryPath;
   }
 
@@ -209,50 +209,6 @@ public abstract class OperaDesktopDriverTestCase {
     c.setCapability('opera.idle', true) if self.settings[:opera_idle] or ENV['OPERA_IDLE'].truthy?
 */
     return c;
-  }
-
-  protected void setEnvVariable(String varName, String varValue)
-  {
-    Map<String, String> env = new HashMap<String, String>();
-    env.put(varName, varValue);
-    setEnv(env);
-  }
-
-  protected void setEnv(Map<String, String> newenv)
-  {
-    try
-    {
-      Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
-      Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
-      theEnvironmentField.setAccessible(true);
-      Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
-      env.putAll(newenv);
-      Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
-      theCaseInsensitiveEnvironmentField.setAccessible(true);
-      Map<String, String> cienv = (Map<String, String>)     theCaseInsensitiveEnvironmentField.get(null);
-      cienv.putAll(newenv);
-    }
-    catch (NoSuchFieldException e)
-    {
-      try {
-        Class[] classes = Collections.class.getDeclaredClasses();
-        Map<String, String> env = System.getenv();
-        for(Class cl : classes) {
-          if("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
-            Field field = cl.getDeclaredField("m");
-            field.setAccessible(true);
-            Object obj = field.get(env);
-            Map<String, String> map = (Map<String, String>) obj;
-            map.clear();
-            map.putAll(newenv);
-          }
-        }
-      } catch (Exception e2) {
-        e2.printStackTrace();
-      }
-    } catch (Exception e1) {
-      e1.printStackTrace();
-    }
   }
 
   /**
