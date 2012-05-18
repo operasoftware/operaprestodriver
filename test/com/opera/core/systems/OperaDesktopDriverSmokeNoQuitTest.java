b0d26f709cc53089ee94c7fbabc92450d2762fdd
Michal Zajaczkowski <mzajaczkowski@opera.com>
2012-05-18 11:36:47 +0200

    Separate the tests into own classes.

---
+++ test/com/opera/core/systems/OperaDesktopDriverSmokeNoQuitTest.java
@@ -0,0 +1,127 @@
+/*
+Copyright 2012 Opera Software ASA
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package com.opera.core.systems;
+
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class OperaDesktopDriverSmokeNoQuitTest extends OperaDesktopDriverTestCase {
+  /**
+   * Attempt to start the driver with the 'opera.no_quit' capability set to true.
+   * This is what happens when you pass the '-q' parameter to desktopwatir.
+   * This test checks that the driver reports Opera running.
+   */
+  @Test
+  public void testStartsOK() {
+    DesiredCapabilities caps = getDefaultCaps();
+    caps.setCapability("opera.no_quit", true);
+    setOperaPath();
+    driver = new TestOperaDesktopDriver(caps);
+    browserPid = driver.getPid();
+
+    assertTrue("Driver doesn't report the Opera as running even thou it should.", driver.isOperaRunning());
+  }
+
+  /**
+   * Check that the PID reported by the driver looks good.
+   */
+  @Test
+  public void testPidOK()
+  {
+    assertTrue("PID reported by browser should > 0", browserPid > 0);
+  }
+
+  /**
+   * Check that the PID reported by the driver refers to an actually running process.
+   * @throws java.io.IOException
+   * @throws InterruptedException
+   */
+  @Test
+  public void testBrowserIsRunning() throws IOException, InterruptedException {
+    assertTrue("The browser process should BE running.", isPidRunning(browserPid));
+  }
+
+  /**
+   * Check that the path reported by the driver is the same as the path that we set for
+   * the OPERA_PATH environment variable override.
+   *
+   * NOTE: This test is known to fail if the OPERA_PATH path points to a shell script that
+   * starts the actual Opera binary. This is what happens on Linux Debian and there is not
+   * much we can do about, we'd have to parse the shell script to get the target path.
+   * It was not verified how the test behaves with symlinks.
+   * @throws IOException
+   */
+  @Test
+  public void testOperaPathReportedOK() throws IOException {
+    String readPath = driver.getOperaPath();
+    File operaPathFile = new File(operaPath).getCanonicalFile();
+    File readFile = new File(readPath).getCanonicalFile();
+
+    assertEquals(operaPathFile, readFile);
+  }
+
+  /**
+   * Check that the capabilities reported by the driver look as we expect.
+   * The actual values may probably change without hurting the general driver
+   * functionality, however this test will detect any changes and allow
+   * inspecting them.
+   */
+  @Test
+  public void testCapsOK() {
+    Map<String, String> expectedValues = new HashMap<String, String>();
+    expectedValues.put("opera.binary", operaPath);
+    expectedValues.put("javascriptEnabled", "true");
+    expectedValues.put("opera.host", "127.0.0.1");
+    expectedValues.put("opera.idle", "false");
+    expectedValues.put("browserName", "opera");
+    expectedValues.put("opera.autostart", "true");
+    expectedValues.put("opera.logging.file", "(null)");
+    expectedValues.put("opera.guess_binary_path", "false");
+    expectedValues.put("opera.logging.level", "INFO");
+    expectedValues.put("opera.launcher", "(null)");
+    expectedValues.put("opera.display", "(null)");
+    expectedValues.put("opera.product", "desktop");
+    expectedValues.put("opera.arguments", "");
+    expectedValues.put("opera.no_quit", "true");
+    expectedValues.put("opera.no_restart", "false");
+
+    checkCaps(driver.getCapabilities(), expectedValues);
+  }
+
+  /**
+   * Check that the browser did not quit after quitting the driver.
+   * This allows to check if the 'opera.no_quit' capability is respected.
+   * @throws IOException
+   * @throws InterruptedException
+   */
+  @Test
+  public void testBrowserDoesNotQuit() throws IOException, InterruptedException {
+    driver.quit();
+    driver = null;
+
+    Boolean isPidRunning = isPidRunning(browserPid);
+    assertTrue("The browser process should BE running.", isPidRunning);
+  }
+}
