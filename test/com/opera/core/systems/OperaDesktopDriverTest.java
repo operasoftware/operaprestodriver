ce0d8674bce956dfad6e867f572745514a8cb806
Andreas Tolf Tolfsen <andreastt@opera.com>
2012-06-01 14:17:03 +0200

    Integrating moar tests

---
+++ test/com/opera/core/systems/OperaDesktopDriverTest.java
@@ -0,0 +1,104 @@
+/*
+Copyright 2012 Opera Software ASA
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package com.opera.core.systems;
+
+import com.opera.core.systems.scope.internal.OperaIntervals;
+import com.opera.core.systems.testing.Ignore;
+import com.opera.core.systems.testing.NoDriver;
+import com.opera.core.systems.testing.OperaDesktopDriverTestCase;
+import com.opera.core.systems.testing.drivers.TestOperaDesktopDriver;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.opera.core.systems.OperaProduct.CORE;
+import static com.opera.core.systems.OperaProduct.MINI;
+import static com.opera.core.systems.OperaProduct.MOBILE;
+import static com.opera.core.systems.OperaProduct.SDK;
+import static com.opera.core.systems.OperaSettings.Capability.PROFILE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@NoDriver
+@Ignore(products = {CORE, MINI, MOBILE, SDK})
+public class OperaDesktopDriverTest extends OperaDesktopDriverTestCase {
+
+  public final long defaultHandshakeTimeout = OperaIntervals.HANDSHAKE_TIMEOUT.getValue();
+
+  public TestOperaDesktopDriver driver;
+  public DesiredCapabilities capabilities;
+  public File profileDirectory;
+
+  @Rule
+  public TemporaryFolder tmp = new TemporaryFolder();
+
+  @Before
+  public void beforeEach() throws IOException {
+    OperaIntervals.HANDSHAKE_TIMEOUT.setValue(2000);
+    capabilities = (DesiredCapabilities) driver.getCapabilities();
+    profileDirectory = tmp.newFolder();
+    capabilities.setCapability(PROFILE.getCapability(), profileDirectory.getPath());
+  }
+
+  @After
+  public void afterEach() {
+    if (driver != null && driver.isRunning()) {
+      driver.quit();
+    }
+    OperaIntervals.HANDSHAKE_TIMEOUT.setValue(defaultHandshakeTimeout);
+  }
+
+  @Test
+  public void specificProfile() throws IOException {
+    driver = new TestOperaDesktopDriver(capabilities);
+
+    assertTrue(driver.isRunning());
+    assertEquals(profileDirectory.getCanonicalPath(),
+                 driver.getSettings().profile().getDirectory().getCanonicalPath());
+  }
+
+  @Test
+  public void startAndQuitOperaFourTimes() throws IOException {
+    assertTrue("Opera should be running", driver.isRunning());
+
+    for (int i = 0; i < 5; i++) {
+      driver = new TestOperaDesktopDriver();
+      assertTrue("Opera should be running", driver.isRunning());
+
+      driver.resetOperaPrefs("");
+      assertTrue("Opera should be running", driver.isRunning());
+
+      driver.quit();
+    }
+  }
+
+  @Test(expected = WebDriverException.class)
+  public void autostartDisabled() {
+    OperaSettings settings = new OperaSettings();
+    settings.autostart(false);
+    new TestOperaDesktopDriver(settings);
+  }
+
+}
