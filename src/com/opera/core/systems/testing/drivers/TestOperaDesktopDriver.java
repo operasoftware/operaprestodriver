b4d7c192fff33f1f996d829766cc9c8f857bd536
Andreas Tolf Tolfsen <andreastt@opera.com>
2012-06-01 14:15:19 +0200

    Adding constructors for the TestOperaDesktopDriver

--- src/com/opera/core/systems/testing/drivers/TestOperaDesktopDriver.java
+++ src/com/opera/core/systems/testing/drivers/TestOperaDesktopDriver.java
@@ -5,15 +5,25 @@ import com.opera.core.systems.OperaSettings;
 import com.opera.core.systems.ScopeServices;
 import com.opera.core.systems.runner.interfaces.OperaRunner;
 
+import org.openqa.selenium.Capabilities;
+
 public class TestOperaDesktopDriver extends OperaDesktopDriver implements TestDriver {
+
+  public TestOperaDesktopDriver() {
+    super();
+  }
+
+  public TestOperaDesktopDriver(Capabilities capabilities) {
+    super(capabilities);
+  }
 
   public TestOperaDesktopDriver(OperaSettings settings) {
     super(settings);
   }
 
   /**
-   * Determines if Opera is running or not.  If Opera is not run locally using {@link com.opera.core.systems.runner.OperaRunner}
-   * it will check for a Scope connection instead.
+   * Determines if Opera is running or not.  If Opera is not run locally using {@link
+   * com.opera.core.systems.runner.OperaRunner} it will check for a Scope connection instead.
    *
    * @return true if Opera is running, false otherwise
    */
