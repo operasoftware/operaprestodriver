7bf2e1fe7437d80c4841b6b8c8fdaae8f6ec1f6b
Andreas Tolf Tolfsen <andreastt@opera.com>
2012-06-07 11:43:30 +0200

    Formatted file

--- src/com/opera/core/systems/util/WatirUtils.java
+++ src/com/opera/core/systems/util/WatirUtils.java
@@ -16,31 +16,30 @@ limitations under the License.
 
 package com.opera.core.systems.util;
 
+import com.google.common.io.Files;
+
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 
-import com.google.common.io.Files;
-
 /**
- * WatirUtils
- *
  * This class contains any utility methods that doesn't really seem to fit anywhere else.
+ *
+ * TODO(andreastt): This needs to go
  */
 public class WatirUtils {
 
-  protected static final Logger logger = Logger.getLogger("WatirUtils");
-  public static final String ANY_MATCHER = "_ANY_";
+  private static final Logger logger = Logger.getLogger(WatirUtils.class.getName());
+  private static final String ANY_MATCHER = "_ANY_";
 
   /**
-   * Returns the platform double click timeout, that is the time that separates two clicks
-   * treated as a double click from two clicks treated as two single clicks.
-   *
+   * Returns the platform double click timeout, that is the time that separates two clicks treated
+   * as a double click from two clicks treated as two single clicks.
    */
-  public static final Integer getSystemDoubleClickTimeMs() {
-    final Integer DEFAULT_INTERVAL_MS = 500;
+  public static Integer getSystemDoubleClickTimeMs() {
+    Integer DEFAULT_INTERVAL_MS = 500;
     Toolkit t = Toolkit.getDefaultToolkit();
 
     if (t == null) {
@@ -52,20 +51,18 @@ public class WatirUtils {
       return DEFAULT_INTERVAL_MS;
     }
 
-    return (Integer)o;
+    return (Integer) o;
   }
 
   /**
-   * Sanitize the given filename/path, so that:
-   * 1. It is not surrounded with quotation marks ""
-   * 2. All occurences of "\\" are changed to a "/"
-   * 3. All occurences of "\" are changed to a "/"
+   * Sanitize the given filename/path, so that (1) it is not surrounded with quotation marks "", (2)
+   * all occurences of "\\" are changed to a "/", and (3) all occurences of "\" are changed to a
+   * "/".
    *
-   * @param input - the input path
-   * @return A StringBuffer containing the sanitized input path
+   * @param input the input path
+   * @return a StringBuffer containing the sanitized input path
    */
-  public static StringBuffer sanitizePath(String input)
-  {
+  public static StringBuffer sanitizePath(String input) {
     StringBuffer buf = new StringBuffer();
 
     // Strip any surrounding quotation marks, that might have came with the file name from
@@ -74,8 +71,8 @@ public class WatirUtils {
       input = input.substring(1, input.length() - 1);
     }
 
-    // Make sure we use "/" as the path separator, seems to be the best solution.
-    // Also, strip double "\\" used as separators.
+    // Make sure we use "/" as the path separator, seems to be the best solution. Also, strip double
+    // "\\" used as separators.
     input = input.replaceAll("\\\\\\\\", "/");
     input = input.replaceAll("\\\\", "/");
 
@@ -84,76 +81,73 @@ public class WatirUtils {
   }
 
   /**
-   * Copies the whole disk directory/file structure starting from the source path to the
-   * destination path.
+   * Copies the whole disk directory/file structure starting from the source path to the destination
+   * path.
    *
-   * @param source - The source path, may designate either a file or a directory
-   * @param destination - The destination path
+   * @param source      the source path, may designate either a file or a directory
+   * @param destination the destination path
    */
   public static boolean copyDirAndFiles(File source, File destination) {
-    logger.finest(String.format("WatirUtils::copyDirAndFiles(%s, %s)", source.getAbsolutePath(), destination.getAbsolutePath()));
+    logger.finest(String.format("WatirUtils::copyDirAndFiles(%s, %s)", source.getAbsolutePath(),
+                                destination.getAbsolutePath()));
 
     if (source.isDirectory()) {
       String[] items;
       items = source.list();
-      for (String item: items) {
+      for (String item : items) {
         File itemSource = new File(source.getPath() + File.separator + item);
         File itemDestination = new File(destination.getPath() + File.separator + item);
         boolean res = copyDirAndFiles(itemSource, itemDestination);
-        if (res == false) {
-          logger.severe("Could not copy '" + itemSource.getAbsolutePath() + "' to '" + itemDestination.getAbsolutePath() + "'!");
+        if (!res) {
+          logger.severe(String.format("Could not copy \"%s\" to \"%s\"",
+                                      itemSource.getPath(), itemDestination.getPath()));
           return false;
         }
       }
-    }
-    else {
+    } else {
       try {
         Files.createParentDirs(destination);
         Files.copy(source, destination);
       } catch (IOException e) {
-        logger.severe(String.format("Could not copy files from \"%s\" to \"%s\"", source, destination));
+        logger.severe(
+            String.format("Could not copy files from \"%s\" to \"%s\"",
+                          source.getPath(), destination.getPath()));
         return false;
       }
     }
+
     return true;
   }
 
   /**
-   * Compares haystack and needle taking into the account that the needle may contain
-   * any number of ANY_MATCHER occurrences, that will be matched to any substring in
-   * haystack, i.e. "Show _ANY_ more..." will match anything like "Show 1 more...", "Show 2 more..." and
-   * so on.
+   * Compares haystack and needle taking into the account that the needle may contain any number of
+   * ANY_MATCHER occurrences, that will be matched to any substring in haystack, i.e. "Show _ANY_
+   * more..." will match anything like "Show 1 more...", "Show 2 more..." and so on.
    *
-   * @param haystack The text that will be compared, may not contain any ANY_MATCHER occurrence
-   * @param needle The text that will be used for comparision, may contain any number of ANY_MATCHER occurrences
-   * @return A boolean indicating whether needle matched haystack.
+   * @param haystack the text that will be compared, may not contain any ANY_MATCHER occurrence
+   * @param needle   the text that will be used for comparision, may contain any number of
+   *                 ANY_MATCHER occurrences
+   * @return a boolean indicating whether needle matched haystack.
    */
-  public static boolean textMatchesWithANY(String haystack, String needle)
-  {
+  public static boolean textMatchesWithANY(String haystack, String needle) {
     haystack = haystack.trim();
     needle = needle.trim();
 
-    /*
-    Make sure we escape every character that is considered to be a special character inside a regular expression.
-    Matcher.quoteReplacement() takes care of '\\' and '$'.
-     */
+    // Make sure we escape every character that is considered to be a special character inside a
+    // regular expression.  Matcher.quoteReplacement() takes care of '\\' and '$'.
     needle = Matcher.quoteReplacement(needle);
     String chars_to_be_escaped = ".|*?+(){}[]^";
 
-    for (char c : chars_to_be_escaped.toCharArray())
-    {
+    for (char c : chars_to_be_escaped.toCharArray()) {
       String regex = "\\" + c;
       String replacement = "\\\\" + c;
       needle = needle.replaceAll(regex, replacement);
     }
 
     String pattern = needle.replaceAll(ANY_MATCHER, "(?:.+)");
-    logger.finest("Looking for pattern '"+ pattern +"' in '" + haystack + "'");
+    logger.finest("Looking for pattern '" + pattern + "' in '" + haystack + "'");
 
-    if (haystack.matches(pattern)) {
-      return true;
-    }
-
-    return false;
+    return haystack.matches(pattern);
   }
+
 }
