4f8f1010ad2e0bf8ddaace18502a6533aedfe6c8
Andreas Tolf Tolfsen <andreastt@opera.com>
2012-08-27 15:30:26 +0200

    Implement Logs ourselves until Selenium 2.26 is out

--- src/com/opera/core/systems/OperaLogs.java
+++ src/com/opera/core/systems/OperaLogs.java
@@ -16,27 +16,72 @@ limitations under the License.
 
 package com.opera.core.systems;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
 import com.opera.core.systems.scope.internal.ServiceCallback;
 import com.opera.core.systems.scope.protos.ConsoleLoggerProtos.ConsoleMessage;
 
+import org.openqa.selenium.logging.LogEntries;
 import org.openqa.selenium.logging.LogEntry;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.Logs;
-import org.openqa.selenium.logging.StoringLocalLogs;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
 
-public class OperaLogs extends StoringLocalLogs implements Logs {
+// TODO(andreastt): Extend StoringLocalLogs when Selenium 2.26 is out
+public class OperaLogs implements Logs {
+
+  private final Map<String, List<LogEntry>> localLogs = Maps.newHashMap();
+  private final Set<String> logTypesToIgnore;
 
   public OperaLogs() {
-    super(new HashSet<String>());
+    this(new HashSet<String>());
   }
 
   public OperaLogs(Set<String> logTypesToIgnore) {
-    super(logTypesToIgnore);
+    this.logTypesToIgnore = logTypesToIgnore;
+  }
+
+  public LogEntries get(String logType) {
+    Iterable<LogEntry> toReturn = getLocalLogs(logType);
+    localLogs.remove(logType);
+    return new LogEntries(toReturn);
+  }
+
+  private Iterable<LogEntry> getLocalLogs(String logType) {
+    if (localLogs.containsKey(logType)) {
+      return localLogs.get(logType);
+    }
+
+    return Lists.newArrayList();
+  }
+
+  /**
+   * Add a new log entry to the local storage.
+   *
+   * @param logType the log type to store
+   * @param entry   the entry to store
+   */
+  public void addEntry(String logType, LogEntry entry) {
+    if (logTypesToIgnore.contains(logType)) {
+      return;
+    }
+
+    if (!localLogs.containsKey(logType)) {
+      localLogs.put(logType, Lists.newArrayList(entry));
+    } else {
+      localLogs.get(logType).add(entry);
+    }
+  }
+
+  public Set<String> getAvailableLogTypes() {
+    return localLogs.keySet();
   }
 
   /**
