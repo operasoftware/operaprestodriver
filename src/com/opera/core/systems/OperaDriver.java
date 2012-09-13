4daf735c88e8a80d39c867aa87cd56ede7c2b5ef
Daniel Bengtsson <danielb@opera.com>
2012-09-13 15:00:23 +0200

    Neither of these prefs exists on mobile

--- src/com/opera/core/systems/OperaDriver.java
+++ src/com/opera/core/systems/OperaDriver.java
@@ -229,11 +229,8 @@ public class OperaDriver extends RemoteWebDriver implements TakesScreenshot, Run
       preferences = new OperaScopePreferences(services.getPrefs());
 
       // Enable popups for testing purposes
-      preferences().set("User Prefs", "Ignore Unrequested Popups", false);
-
-      // Mobile has disabled JavaScript autofocus for usability reasons
-      if (utils().getProduct().is(MOBILE)) {
-        preferences().set("User Prefs", "Allow Autofocus Form Element", true);
+      if (!utils().getProduct().is(MOBILE)) {
+        preferences().set("User Prefs", "Ignore Unrequested Popups", false);
       }
     }
 
