ec58b7673e592610598a891e02d6a474ba4857df
Daniel Bengtsson <danielb@opera.com>
2012-09-14 11:55:24 +0200

    Fix OperaArguments.remove
    The current implementation was based on the objects and as we had no equals()
    implementation it failed to remove. I let eclise generate the functions as a
    quick fix to make it work. But this should be cleaned up and probably solved
    differently as it's all just a workaround for moving -fullscreen last on mac.

--- src/com/opera/core/systems/arguments/OperaArgument.java
+++ src/com/opera/core/systems/arguments/OperaArgument.java
@@ -136,4 +136,24 @@ public class OperaArgument {
     return (key.length() > sign.length()) && key.substring(0, sign.length()).equals(sign);
   }
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((argument == null) ? 0 : argument.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    OperaArgument other = (OperaArgument) obj;
+    if (argument == null) {
+      if (other.argument != null) return false;
+    } else if (!argument.equals(other.argument)) return false;
+    return true;
+  }
+
 }
