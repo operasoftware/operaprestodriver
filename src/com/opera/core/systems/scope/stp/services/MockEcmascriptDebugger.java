b17282c757d50c538135cd2dac91edf27c85a033
Andreas Tolf Tolfsen <andreastt@opera.com>
2012-10-05 17:21:31 +0200

    Add copyright header

--- src/com/opera/core/systems/scope/stp/services/MockEcmascriptDebugger.java
+++ src/com/opera/core/systems/scope/stp/services/MockEcmascriptDebugger.java
@@ -1,3 +1,19 @@
+/*
+Copyright 2012 Opera Software ASA
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package com.opera.core.systems.scope.stp.services;
 
 import com.opera.core.systems.OperaDriver;
@@ -6,6 +22,7 @@ import com.opera.core.systems.scope.protos.EcmascriptProtos;
 import com.opera.core.systems.scope.protos.Esdbg6Protos;
 import com.opera.core.systems.scope.protos.EsdbgProtos;
 import com.opera.core.systems.scope.services.Debugger;
+import com.opera.core.systems.scope.services.EcmascriptDebugger;
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -19,14 +36,11 @@ public class MockEcmascriptDebugger implements Debugger {
   }
 
   public String getServiceName() {
-    return "ecmascript-debugger";
+    return EcmascriptDebugger.SERVICE_NAME;
   }
 
   public String getServiceVersion() {
     return null;
-  }
-
-  public void setRuntime(Esdbg6Protos.RuntimeInfo runtime) {
   }
 
   public Object scriptExecutor(String script, Object... params) {
@@ -132,10 +146,6 @@ public class MockEcmascriptDebugger implements Debugger {
 
   public boolean isScriptInjectable() {
     return false;
-  }
-
-  public int compareTo(Service service) {
-    return 0;
   }
 
 }
